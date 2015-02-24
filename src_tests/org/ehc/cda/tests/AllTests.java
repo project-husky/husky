d5d78bd8af14a5054e7139aa2333026847365d4d
Oliver Egger <oliver.egger@ahdis.ch>
2015-02-24 20:34:17 +0000

    refactoring of tests and adjusting build path
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@227 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

---
+++ src_tests/org/ehc/cda/tests/AllTests.java
@@ -0,0 +1,35 @@
+package org.ehc.cda.tests;
+import java.util.Random;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * <div class="de">Erzeugt ein AllTests.java- Objekt.</div>
+ * <div class="fr"></div>
+ * <div class="it"></div>
+ */
+
+/**
+ * @author hodi
+ * Test 
+ */
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class AllTests {
+
+	
+	public static String generateString(int length)
+	{
+		Random rng = new Random();
+		String characters = "abcëÙdÀÿeŒfúgËÛùhijàÊkÇlŸmœÏçÚnÔÈoæûèp»ÙÈqùôêîïÆrsâÉtéÎuvwèxylïäüìöÄÏÒÜÂÖÌ?ßÓ/òó:#\\íÍ~*É'é,´Àà";
+		
+	    char[] text = new char[length];
+	    for (int i = 0; i < length; i++)
+	    {
+	        text[i] = characters.charAt(rng.nextInt(characters.length()));
+	    }
+	    return new String(text);
+	}
+}
