11283e5058de78140b778b52ee3a259d74bebcbc
Oliver Egger <oliver.egger@ahdis.ch>
2015-07-08 09:37:11 +0000

    Adding one Metadata CH Confidentiality Code
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@463 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- src/org/ehealth_connector/cda/enums/Confidentiality.java
+++ src/org/ehealth_connector/cda/enums/Confidentiality.java
@@ -15,49 +15,58 @@ public enum Confidentiality {
 	/**
 	 * <div class="de">Streng vertraulich</div> <div class="fr"></div>
 	 */
-	VERY_RESTRICTED("VR", "Very restricted"),
-	/**
-	 * <div class="de">Vertraulich</div> <div class="fr"></div>
-	 */
-	RESTRICED("R", "Restriced"),
-	/**
-	 * <div class="de">Normal</div> <div class="fr"></div>
-	 */
-	NORMAL("N", "Normal");
+	VERY_RESTRICTED("VR", "2.16.840.1.113883.5.25",
+			"Very restricted"), /**
+								 * <div class="de">Vertraulich </div>
+								 * <div class="fr"></div>
+								 */
+	RESTRICED("R", "2.16.840.1.113883.5.25",
+			"Restriced"), /**
+							 * <div class="de">Normal</div>
+							 * <div class="fr"></div>
+							 */
+	NORMAL("N", "2.16.840.1.113883.5.25", "Normal"),
+
+	CH_30003("30003", "2.16.756.5.30.1.127.3.10.1.5", "medizinische Daten");
+
 	public static final String VERY_RESTRICTED_CODE = "VR";
 	public static final String RESTRICED_CODE = "R";
 	public static final String NORMAL_CODE = "N";
 
-	public static final String CODE_SYSTEM_OID = "2.16.840.1.113883.5.25";
+	// public static final String CODE_SYSTEM_OID = "2.16.840.1.113883.5.25";
 	public static final String CODE_SYSTEM_NAME = "Confidentiality";
+
+	public static final String CODE_SYSTEM_OID_CH = "2.16.756.5.30.1.127.3.10.1.5";
 
 	private String code;
 	private String displayName;
+	private String oid;
 
 	/**
-	 * <div class="en">Instantiates this Enum Object with a given Code and Display
-	 * Name</div> <div class="de">Instantiiert dieses Enum Object mittels eines
-	 * Codes und einem Display Name</div>
+	 * <div class="en">Instantiates this Enum Object with a given Code and
+	 * Display Name</div> <div class="de">Instantiiert dieses Enum Object
+	 * mittels eines Codes und einem Display Name</div>
 	 * 
 	 * @param code
-	 * <br>
-	 *          <div class="de"> code</div>
+	 *            <br>
+	 *            <div class="de"> code</div>
 	 * @param displayName
-	 * <br>
-	 *          <div class="de"> display name</div>
+	 *            <br>
+	 *            <div class="de"> display name</div>
 	 */
-	private Confidentiality(String code, String displayName) {
+	private Confidentiality(String code, String oid, String displayName) {
 		this.code = code;
 		this.displayName = displayName;
+		this.oid = oid;
 	}
 
 	/**
-	 * <div class="en">Gets the Enum with a given code</div> <div
-	 * class="de">Liefert den Enum anhand eines gegebenen codes</div>
+	 * <div class="en">Gets the Enum with a given code</div>
+	 * <div class="de">Liefert den Enum anhand eines gegebenen codes</div>
 	 * 
 	 * @param code
-	 * <br>
-	 *          <div class="de"> code</div>
+	 *            <br>
+	 *            <div class="de"> code</div>
 	 * @return <div class="en">the enum</div>
 	 */
 	public static Confidentiality getEnum(String code) {
@@ -70,27 +79,27 @@ public enum Confidentiality {
 	}
 
 	/**
-	 * <div class="en">Gets the Code of this Enum as MDHT Object.</div> <div
-	 * class="de">Liefert den Code dieses Enum als MDHT Objekt.</div>
+	 * <div class="en">Gets the Code of this Enum as MDHT Object.</div>
+	 * <div class="de">Liefert den Code dieses Enum als MDHT Objekt.</div>
 	 * 
 	 * @return <div class="en">The MDHT Code</div>
 	 */
 	public CE getCE() {
 		CE ce = DatatypesFactory.eINSTANCE.createCE();
-		ce.setCodeSystem(CODE_SYSTEM_OID);
+		ce.setCodeSystem(oid);
 		ce.setCode(code);
 		ce.setDisplayName(displayName);
 		return ce;
 	}
 
 	/**
-	 * <div class="en">Gets the ehealthconnector Code Object</div> <div
-	 * class="de">Liefert das ehealthconnector Code Objekt</div>
+	 * <div class="en">Gets the ehealthconnector Code Object</div>
+	 * <div class="de">Liefert das ehealthconnector Code Objekt</div>
 	 * 
 	 * @return <div class="en">the code</div>
 	 */
 	public Code getCode() {
-		Code ehcCode = new Code(CODE_SYSTEM_OID, code, displayName);
+		Code ehcCode = new Code(oid, code, displayName);
 		return ehcCode;
 	}
 
@@ -105,18 +114,18 @@ public enum Confidentiality {
 	}
 
 	/**
-	 * <div class="en">Gets the code system id.</div> <div class="de">Liefert die
-	 * code system id.</div>
+	 * <div class="en">Gets the code system id.</div> <div class="de">Liefert
+	 * die code system id.</div>
 	 * 
 	 * @return <div class="en">the code system id</div>
 	 */
 	public String getCodeSystemOid() {
-		return CODE_SYSTEM_OID;
+		return this.oid;
 	}
 
 	/**
-	 * <div class="en">Gets the actual Code as string</div> <div
-	 * class="de">Liefert den eigentlichen Code als String</div>
+	 * <div class="en">Gets the actual Code as string</div>
+	 * <div class="de">Liefert den eigentlichen Code als String</div>
 	 * 
 	 * @return <div class="en">the code</div>
 	 */
@@ -141,8 +150,8 @@ public enum Confidentiality {
 	 * 
 	 * 
 	 * @param enumName
-	 * <br>
-	 *          <div class="de"> enumName</div>
+	 *            <br>
+	 *            <div class="de"> enumName</div>
 	 * @return true, if enum is in this value set
 	 */
 	public boolean isEnumOfValueSet(String enumName) {
@@ -151,12 +160,12 @@ public enum Confidentiality {
 
 	/**
 	 * <div class="en">Checks if a given code value is in this value set.</div>
-	 * <div class="de">Prüft, ob der gegebene code in diesem Value Sets vorhanden
-	 * ist.</div>
+	 * <div class="de">Prüft, ob der gegebene code in diesem Value Sets
+	 * vorhanden ist.</div>
 	 * 
 	 * @param codeValue
-	 * <br>
-	 *          <div class="de"> code</div>
+	 *            <br>
+	 *            <div class="de"> code</div>
 	 * @return true, if is in value set
 	 */
 	public boolean isInValueSet(String codeValue) {
