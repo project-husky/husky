511f8dc1f76aa9b51f378e853e1d77ff1f695015
Axel Helmer <axel.helmer.job@gmail.com>
2015-10-22 14:53:54 +0000

    - Implemented IndexHtm.java with three languages
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@593 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- src/org/ehealth_connector/communication/xd/xdm/IndexHtm.java
+++ src/org/ehealth_connector/communication/xd/xdm/IndexHtm.java
@@ -39,8 +39,9 @@ public class IndexHtm {
 	/** The inputStream. */
 	private InputStream is = null;
 
-	/** The Constant TEMPLATE_EN. */
-	public static final String TEMPLATE_EN = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+	/** The HTML Header (including headline) */
+	public static final String HTML_HEADER = 
+			"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
 			+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
 			+ "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
 			+ "<head>\n"
@@ -49,15 +50,43 @@ public class IndexHtm {
 			+ "<body bgcolor=\"#F1FFFE\">\n"
 			+ "<div align=\"center\">\n"
 			+ "<h1>eHealthConnector - XDM Cross-Enterprise Document Media Interchange</h1>\n"
-			+ "</div>\n"
-			+ "<h2>Institution</h2>\n"
+			+ "</div>\n";
+	/** The HTML Footer */
+	public static final String HTML_FOOTER = "</body>\n" + "</html>";
+	/** The HTML Separator for different languages inside this file */
+	public static final String HTML_SEPARATOR = "<hr />";
+	
+	/** The Constant TEMPLATE_EN. */
+	public static final String TEMPLATE_EN = 
+			"<h2>Institution</h2>\n"
 			+ "<p>Information about this XDM volume in english.<br/>\n"
 			+ "This XDM volume was created by {0}. </p>\n"
 			+ "<p><a target=\"_blank\" href=\"http://sourceforge.net/p/ehealthconnector/wiki/Team\">{1}</a></p>\n"
 			+ "<h2>Contents</h2>\n{2}"
+			+ "<p>This volume contains {3} Documents in 1 Submission Set.</p>\n"
 			+ "<h3>README File</h3>\n"
-			+ "<p>For technical details about this volume and vendor information, see: <a target=\"_blank\" href=\"README.TXT\">README.TXT</a>.</p>\n"
-			+ "</body>\n" + "</html>";
+			+ "<p>For technical details about this volume and vendor information, see: <a target=\"_blank\" href=\"README.TXT\">README.TXT</a>.</p>\n";	
+			/** The Constant TEMPLATE_DE. */
+	public static final String TEMPLATE_DE = 
+			"<h2>Verwaltende Organisation</h2>\n"
+			+ "<p>Informationen zu diesem XDM Paket in Deutsch.<br/>\n"
+			+ "Dieses XDM Paket wurde erstellt von: {0}. </p>\n"
+			+ "<p><a target=\"_blank\" href=\"http://sourceforge.net/p/ehealthconnector/wiki/Team\">{1}</a></p>\n"
+			+ "<h2>Inhalt</h2>\n{2}"
+			+ "<p>Dieses Paket enthält {3} Dokumente in 1 Submission Set.</p>\n"
+			+ "<h3>README Datei</h3>\n"
+			+ "<p>Technische Details zu diesem XDM Paket und dessen Hersteller befinden sich hier: <a target=\"_blank\" href=\"README.TXT\">README.TXT</a>.</p>\n";
+	/** The Constant TEMPLATE_FR. */
+	public static final String TEMPLATE_FR = 
+			"<h2>Organisme de gestion</h2>\n"
+			+ "<p>Informations sur ce volume XDM en français.<br/>\n"
+			+ "Ce volume XDM a été créée par {0}. </p>\n"
+			+ "<p><a target=\"_blank\" href=\"http://sourceforge.net/p/ehealthconnector/wiki/Team\">{1}</a></p>\n"
+			+ "<h2>Contenu</h2>\n{2}"
+			+ "<p>Ce volume contient {3} Dokumente en 1 Submission Set.</p>\n"
+			+ "<h3>Fichier README</h3>\n"
+			+ "<p>Pour plus de informations techniques au sujet de ce volume et le fournisseur, voir: <a target=\"_blank\" href=\"README.TXT\">README.TXT</a>.</p>\n";
+
 
 	/**
 	 * Instantiates a new indexHtm.
@@ -91,10 +120,23 @@ public class IndexHtm {
 	public IndexHtm(SubmitTransactionData txnData, VendorInformation vendorInfo) {
 
 		logService.debug("Creating the Index.htm file");
-
-		Object[] values = new Object[] { vendorInfo.getVendorName(),
-				vendorInfo.getContactInformation(), createContents(txnData) };
-		String str = MessageFormat.format(TEMPLATE_EN, values);
+		Object[] values;
+		String str;
+		
+		//Add html header
+		str = HTML_HEADER;
+		
+		//Add html body with dynamic content
+		values = new Object[] { vendorInfo.getVendorName(),
+				vendorInfo.getContactInformation(), createContents(txnData),txnData.getDocList().size()};
+		str = str + MessageFormat.format(TEMPLATE_EN, values);
+		str = str + HTML_SEPARATOR;
+		str = str + MessageFormat.format(TEMPLATE_DE, values);
+		str = str + HTML_SEPARATOR;
+		str = str + MessageFormat.format(TEMPLATE_FR, values);
+		
+		str = str + HTML_FOOTER;
+		
 		is = new ByteArrayInputStream(str.getBytes());
 	}
 
@@ -116,8 +158,7 @@ public class IndexHtm {
 	 */
 	private String createContents(SubmitTransactionData txnData) {
 		// Number of Items
-		String contentsStr = "<p>This volume contains " + +txnData.getDocList().size()
-				+ " Documents in 1 Submission Set:</p>\n" + "<h3>Submission-Set 1</h3>\n"
+		String contentsStr = "<h3>Submission-Set 1</h3>\n"
 				+ "<p>Patient ID: " + txnData.getSubmissionSet().getPatientId().getIdNumber()
 				+ "</p>\n" + "<ul>\n";
 
