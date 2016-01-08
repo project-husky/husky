b14512fb36907c3fc80bc76d6eddeb066522d707
Roeland Luykx <luykx@arpage.ch>
2016-01-08 14:23:02 +0000

    new unit tests
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@731 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

---
+++ ehealth_connector-cda/ehealth_connector-cda-ch/src/test/java/org/ehealth_connector/cda/ch/enums/RiskOfComplicationsTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ *
+ * The authorship of this code and the accompanying materials is held by medshare GmbH, Switzerland.
+ * All rights reserved. http://medshare.net
+ *
+ * Project Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/
+ *
+ * This code is are made available under the terms of the Eclipse Public License v1.0.
+ *
+ * Accompanying materials are made available under the terms of the Creative Commons
+ * Attribution-ShareAlike 4.0 License.
+ *
+ * Year of publication: 2016
+ *
+ *******************************************************************************/
+package org.ehealth_connector.cda.ch.enums;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.ehealth_connector.cda.enums.LanguageCode;
+import org.ehealth_connector.common.Code;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author roeland
+ * @version 1.0
+ * @since Jan 8, 2016 2:50:40 PM
+ *
+ */
+public class RiskOfComplicationsTest {
+
+	private LanguageCode testLanguage;
+
+	private RiskOfComplications testRiskOfCom;
+
+	/**
+	 * Method implementing
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		testLanguage = LanguageCode.FRENCH;
+		testRiskOfCom = RiskOfComplications.ANDERE_LUNGENERKRANKUNGEN_ZB_MUKOVISZIDOSE_ASTHMA_BRONCHIALE_ETC;
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.ehealth_connector.cda.ch.enums.RiskOfComplications#getDisplayName(org.ehealth_connector.cda.enums.LanguageCode)}
+	 * .
+	 */
+	@Test
+	public void testGetDisplayNameLanguageCode() {
+		final String displNameFrench = testRiskOfCom.getDisplayName(testLanguage);
+		assertNotNull(displNameFrench);
+		assertEquals("Autres maladies pulmonaires (ex : mucoviscidose, asthme traité par aérosols, etc)", displNameFrench);
+	}
+
+	@Test
+	public void testGetCode() {
+		final Code ref = testRiskOfCom.getCode(testLanguage);
+		assertNotNull(ref);
+		assertEquals("Autres maladies pulmonaires (ex : mucoviscidose, asthme traité par aérosols, etc)", ref.getDisplayName());
+	}
+
+}
---
+++ ehealth_connector-cda/ehealth_connector-cda-ch/src/test/java/org/ehealth_connector/cda/ch/enums/RiskOfExposureTest.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ *
+ * The authorship of this code and the accompanying materials is held by medshare GmbH, Switzerland.
+ * All rights reserved. http://medshare.net
+ *
+ * Project Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/
+ *
+ * This code is are made available under the terms of the Eclipse Public License v1.0.
+ *
+ * Accompanying materials are made available under the terms of the Creative Commons
+ * Attribution-ShareAlike 4.0 License.
+ *
+ * Year of publication: 2016
+ *
+ *******************************************************************************/
+package org.ehealth_connector.cda.ch.enums;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.ehealth_connector.cda.enums.LanguageCode;
+import org.ehealth_connector.common.Code;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author roeland
+ * @version 1.0
+ * @since Jan 8, 2016 3:05:42 PM
+ *
+ */
+public class RiskOfExposureTest {
+
+	private LanguageCode testLanguage;
+
+	private RiskOfExposure testRiskOfExp;
+
+	/**
+	 * Method implementing
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		testLanguage = LanguageCode.FRENCH;
+		testRiskOfExp = RiskOfExposure.FLEDERMAUSFORSCHER_UND_SCHÜTZER;
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.ehealth_connector.cda.ch.enums.RiskOfExposure#getCode(org.ehealth_connector.cda.enums.LanguageCode)}
+	 * .
+	 */
+	@Test
+	public void testGetCode() {
+		final Code ref = testRiskOfExp.getCode(testLanguage);
+		assertNotNull(ref);
+		assertEquals("Scientifiques effectuant des recherches sur les chiroptères, protecteurs ou amateurs de chauves-souris",
+				ref.getDisplayName());
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link org.ehealth_connector.cda.ch.enums.RiskOfExposure#getDisplayName(org.ehealth_connector.cda.enums.LanguageCode)}
+	 * .
+	 */
+	@Test
+	public void testGetDisplayName() {
+		final String displNameFrench = testRiskOfExp.getDisplayName(testLanguage);
+		assertNotNull(displNameFrench);
+		assertEquals("Scientifiques effectuant des recherches sur les chiroptères, protecteurs ou amateurs de chauves-souris",
+				displNameFrench);
+	}
+
+}
