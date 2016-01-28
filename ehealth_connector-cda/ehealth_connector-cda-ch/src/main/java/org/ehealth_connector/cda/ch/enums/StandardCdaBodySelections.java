5b14b2c64d8f39c9d8390e4d22ae24e8b9387a8a
Axel Helmer <axel.helmer.job@gmail.com>
2016-01-28 09:56:38 +0000

    - Added Enums for LRPH
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@793 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

---
+++ ehealth_connector-cda/ehealth_connector-cda-ch/src/main/java/org/ehealth_connector/cda/ch/enums/StandardCdaBodySelections.java
@@ -0,0 +1,202 @@
+package org.ehealth_connector.cda.ch.enums;
+
+import org.ehealth_connector.common.Code;
+import org.openhealthtools.mdht.uml.hl7.datatypes.CD;
+import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
+
+/*
+*<div class="en">Structured Yes/No Answer</div>
+*<div class="de">Strukturierte Ja/Nein Antwort</div>
+*/
+public enum StandardCdaBodySelections {
+
+	/**
+	 * <div class="en">Brucella abortus</div>
+	 */
+	NO("N", "No"),
+	/**
+	 * <div class="en">Brucella</div>
+	 */
+	YES("Y", "Yes");
+
+	/**
+	 * <div class="en">Name of the Code System</div> <div class="de">Name des
+	 * Codes Systems</div>
+	 */
+	public static final String CODE_SYSTEM_NAME = "StandardCDABodySelektion";
+
+	/**
+	 * <div class="en">Identifier of the Code System</div>
+	 * <div class="de">Identifikator für das Code System</div>
+	 */
+	public static final String CODE_SYSTEM_OID = "2.16.756.5.30.2.1.1.3.1";
+
+	/**
+	 * <div class="en">Gets the Enum with a given code</div>
+	 * <div class="de">Liefert den Enum anhand eines gegebenen codes</div>
+	 *
+	 * @param code
+	 *          <br>
+	 *          <div class="de"> code</div>
+	 * @return <div class="en">the enum</div>
+	 */
+	public static StandardCdaBodySelections getEnum(String code) {
+		for (StandardCdaBodySelections x : values()) {
+			if (x.getCodeValue().equals(code)) {
+				return x;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * <div class="en">Checks if a given enum is part of this value set.</div>
+	 * <div class="de">Prüft, ob der gegebene enum Teil dieses Value Sets
+	 * ist.</div>
+	 *
+	 *
+	 * @param enumName
+	 *          <br>
+	 *          <div class="de"> enumName</div>
+	 * @return true, if enum is in this value set
+	 */
+	public static boolean isEnumOfValueSet(String enumName) {
+		if (enumName == null) {
+			return false;
+		}
+		try {
+			Enum.valueOf(StandardCdaBodySelections.class, enumName);
+			return true;
+		} catch (final IllegalArgumentException ex) {
+			return false;
+		}
+	}
+
+	/**
+	 * <div class="en">Checks if a given code value is in this value set.</div>
+	 * <div class="de">Prüft, ob der gegebene code in diesem Value Set vorhanden
+	 * ist.</div>
+	 *
+	 * @param codeValue
+	 *          <br>
+	 *          <div class="de"> code</div>
+	 * @return true, if is in value set
+	 */
+	public static boolean isInValueSet(String codeValue) {
+		for (StandardCdaBodySelections x : values()) {
+			if (x.getCodeValue().equals(codeValue)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * <div class="en">Machine interpretable and (inside this class) unique
+	 * code</div> <div class="de">Maschinen interpretierbarer und (innerhalb
+	 * dieser Klasse) eindeutiger Code</div>
+	 */
+	protected String code;
+
+	/**
+	 * <div class="en">Human readable name</div> <div class="de">Menschenlesbarer
+	 * Name</div>
+	 */
+	protected String displayName;
+
+	/**
+	 * <div class="en">Instantiates this Enum Object with given code, displayName,
+	 * specialitySectionCode, privacyFilter, validFrom and validTo
+	 * parameters</div> <div class="de">Instantiiert dieses Enum Object mittels
+	 * der code, displayName, specialitySectionCode, privacyFilter, validFrom und
+	 * validTo Parameter</div>
+	 *
+	 * @param code
+	 *          <br>
+	 *          <div class="en">the code</div>
+	 * @param displayName
+	 *          <br>
+	 *          <div class="en">the display name</div>
+	 * @param sectionCode
+	 *          <br>
+	 *          <div class="en">the specialty section code</div>
+	 * @param patientPrivacyFilter
+	 *          <br>
+	 *          <div class="en">the patient privacy filter</div>
+	 * @param validFrom
+	 *          <br>
+	 *          <div class="en">the valid from</div>
+	 * @param validTo
+	 *          <br>
+	 *          <div class="en">the valid to</div>
+	 */
+	private StandardCdaBodySelections(String code, String displayName) {
+		this.code = code;
+		this.displayName = displayName;
+	}
+
+	/**
+	 * <div class="en">Gets the Code of this Enum as MDHT Object.</div>
+	 * <div class="de">Liefert den Code dieses Enum als MDHT Objekt.</div>
+	 *
+	 * @return <div class="en">The MDHT Code</div>
+	 */
+	public CD getCD() {
+		CD cd = DatatypesFactory.eINSTANCE.createCD();
+		cd.setCodeSystem(CODE_SYSTEM_OID);
+		cd.setCode(code);
+		cd.setDisplayName(displayName);
+		return cd;
+	}
+
+	/**
+	 * <div class="en">Gets the ehealthconnector Code Object</div>
+	 * <div class="de">Liefert das ehealthconnector Code Objekt</div>
+	 *
+	 * @return <div class="en">the code</div>
+	 */
+	public Code getCode() {
+		Code ehcCode = new Code(CODE_SYSTEM_OID, code, displayName);
+		return ehcCode;
+	}
+
+	/**
+	 * <div class="en">Gets the code system name.</div> <div class="de">Liefert
+	 * code system name.</div>
+	 *
+	 * @return <div class="en">the code system name</div>
+	 */
+	public String getCodeSystemName() {
+		return CODE_SYSTEM_NAME;
+	}
+
+	/**
+	 * <div class="en">Gets the code system id.</div> <div class="de">Liefert die
+	 * code system id.</div>
+	 *
+	 * @return <div class="en">the code system id</div>
+	 */
+	public String getCodeSystemOid() {
+		return CODE_SYSTEM_OID;
+	}
+
+	/**
+	 * <div class="en">Gets the actual Code as string</div>
+	 * <div class="de">Liefert den eigentlichen Code als String</div>
+	 *
+	 * @return <div class="en">the code</div>
+	 */
+	public String getCodeValue() {
+		return this.code;
+	}
+
+	/**
+	 * <div class="en">Gets the display name.</div> <div class="de">Liefert
+	 * display name.</div>
+	 *
+	 * @return <div class="en">the display name</div>
+	 */
+	public String getDisplayName() {
+		return this.displayName;
+	}
+}
