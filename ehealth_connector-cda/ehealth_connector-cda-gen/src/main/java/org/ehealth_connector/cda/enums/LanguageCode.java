7ba329a164406cffdeb52e5a896ff9e41a3c1524
Patrick Kistler <pkistler@myrddin.ch>
2016-02-03 08:19:27 +0000

    fix warning (formatter of code is inconsistent in versions of eclipse), return params on new line now
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@809 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-cda/ehealth_connector-cda-gen/src/main/java/org/ehealth_connector/cda/enums/LanguageCode.java
+++ ehealth_connector-cda/ehealth_connector-cda-gen/src/main/java/org/ehealth_connector/cda/enums/LanguageCode.java
@@ -5,25 +5,27 @@ import org.ehealth_connector.common.Code;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CS;
 import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
 
-/*
- *<div class="de">Sprache</div>
- *<div class="fr"></div>
+/**
+ * <div class="de">Sprache</div>
+ * <div class="fr"></div>
  */
 public enum LanguageCode {
 
 	/**
 	 * <div class="de">deutsch</div> <div class="fr"></div>
 	 */
-	GERMAN("de-CH", "german"), /**
-								 * <div class="de">französisch</div>
-								 * <div class="fr"></div>
-								 */
-	FRENCH("fr-CH", "french"), /**
-								 * <div class="de">italienisch</div>
-								 * <div class="fr"></div>
-								 */
+	GERMAN("de-CH", "german"),
+	/**
+	 * <div class="de">französisch</div>
+	 * <div class="fr"></div>
+	 */
+	FRENCH("fr-CH", "french"),
+	/**
+	 * <div class="de">italienisch</div>
+	 * <div class="fr"></div>
+	 */
 	ITALIAN("it-CH", "italian"),
-	
+
 	ENGLISH("en", "english");
 	public static final String GERMAN_CODE = "de-CH";
 	public static final String FRENCH_CODE = "fr-CH";
@@ -40,24 +42,21 @@ public enum LanguageCode {
 	 * <div class="en">Instantiates this Enum Object with a given Code and
 	 * Display Name</div> <div class="de">Instantiiert dieses Enum Object
 	 * mittels eines Codes und einem Display Name</div>
-	 * 
 	 * @param code
-	 *            <br>
-	 *            <div class="de"> code</div>
+	 * <br>
+	 * <div class="de"> code</div>
 	 * @param displayName
-	 *            <br>
-	 *            <div class="de"> display name</div>
+	 * <br>
+	 * <div class="de"> display name</div>
 	 */
 	private LanguageCode(String code, String displayName) {
 		this.code = code;
 		this.displayName = displayName;
 	}
 
-
 	/**
 	 * <div class="en">Gets the ehealthconnector Code Object</div>
 	 * <div class="de">Liefert das ehealthconnector Code Objekt</div>
-	 * 
 	 * @return <div class="en">the code</div>
 	 */
 	public Code getCode() {
@@ -68,7 +67,6 @@ public enum LanguageCode {
 	/**
 	 * <div class="en">Gets the code system name.</div> <div class="de">Liefert
 	 * code system name.</div>
-	 * 
 	 * @return <div class="en">the code system name</div>
 	 */
 	public String getCodeSystemName() {
@@ -78,7 +76,6 @@ public enum LanguageCode {
 	/**
 	 * <div class="en">Gets the code system id.</div> <div class="de">Liefert
 	 * die code system id.</div>
-	 * 
 	 * @return <div class="en">the code system id</div>
 	 */
 	public String getCodeSystemOid() {
@@ -88,7 +85,6 @@ public enum LanguageCode {
 	/**
 	 * <div class="en">Gets the actual Code as string</div>
 	 * <div class="de">Liefert den eigentlichen Code als String</div>
-	 * 
 	 * @return <div class="en">the code</div>
 	 */
 	public String getCodeValue() {
@@ -98,7 +94,6 @@ public enum LanguageCode {
 	/**
 	 * <div class="en">Gets the Code of this Enum as MDHT Object.</div>
 	 * <div class="de">Liefert den Code dieses Enum als MDHT Objekt.</div>
-	 * 
 	 * @return <div class="en">The MDHT Code</div>
 	 */
 	public CS getCS() {
@@ -110,7 +105,6 @@ public enum LanguageCode {
 	/**
 	 * <div class="en">Gets the display name.</div> <div class="de">Liefert
 	 * display name.</div>
-	 * 
 	 * @return <div class="en">the display name</div>
 	 */
 	public String getDisplayName() {
@@ -120,9 +114,8 @@ public enum LanguageCode {
 	/**
 	 * <div class="en">Gets the Enum with a given code</div> <div
 	 * class="de">Liefert den Enum anhand eines gegebenen codes</div>
-	 *
-	 * @param code <br> <div class="de"> code</div> @return <div class="en">the
-	 * enum</div>
+	 * @param code <br> <div class="de"> code</div>
+	 * @return <div class="en">the enum</div>
 	 */
 	public static LanguageCode getEnum(String code) {
 		for (final LanguageCode aLanguage : EnumUtils.getEnumList(LanguageCode.class)) {
@@ -137,10 +130,8 @@ public enum LanguageCode {
 	 * <div class="en">Checks if a given enum is part of this value set.</div>
 	 * <div class="de">Prüft, ob der gegebene enum Teil dieses Value Sets
 	 * ist.</div>
-	 *
-	 *
-	 * @param enumName <br> <div class="de"> enumName</div> @return true, if
-	 * enum is in this value set
+	 * @param enumName <br> <div class="de"> enumName</div>
+	 * @return true, if enum is in this value set
 	 */
 	public static boolean isEnumOfValueSet(String enumName) {
 		return EnumUtils.isValidEnum(LanguageCode.class, enumName);
@@ -150,9 +141,8 @@ public enum LanguageCode {
 	 * <div class="en">Checks if a given code value is in this value set.</div>
 	 * <div class="de">Prüft, ob der gegebene code in diesem Value Sets
 	 * vorhanden ist.</div>
-	 *
-	 * @param codeValue <br> <div class="de"> code</div> @return true, if is in
-	 * value set
+	 * @param codeValue <br> <div class="de"> code</div>
+	 * @return true, if is in value set
 	 */
 	public static boolean isInValueSet(String codeValue) {
 		for (final LanguageCode aLang : EnumUtils.getEnumList(LanguageCode.class)) {
