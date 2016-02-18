638977a70928879829906ab31f01f41bcb2cfaa7
Axel Helmer <axel.helmer.job@gmail.com>
2016-02-18 14:44:42 +0000

    - Added lrqc.LaboratoryBatteryOrganizer
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@844 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

---
+++ ehealth_connector-cda/ehealth_connector-cda-ch/src/test/java/org/ehealth_connector/cda/ch/lab/lrqc/LaboratoryBatteryOrganizer.java
@@ -0,0 +1,32 @@
+package org.ehealth_connector.cda.ch.lab.lrqc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ehealth_connector.cda.ObservationMediaEntry;
+import org.openhealthtools.mdht.uml.cda.ObservationMedia;
+
+public class LaboratoryBatteryOrganizer
+		extends org.ehealth_connector.cda.ch.lab.LaboratoryBatteryOrganizer {
+	public LaboratoryBatteryOrganizer() {
+		super();
+	}
+
+	public LaboratoryBatteryOrganizer(
+			org.openhealthtools.mdht.uml.cda.ihe.lab.LaboratoryBatteryOrganizer mdht) {
+		super(mdht);
+	}
+
+	public void addObservationMediaEntry(ObservationMediaEntry observationMedia) {
+		getMdht().addObservationMedia(observationMedia.copy());
+	}
+
+	public List<ObservationMediaEntry> getObservationMediaEntries() {
+		List<ObservationMediaEntry> ol = new ArrayList<ObservationMediaEntry>();
+		for (ObservationMedia om : getMdht().getObservationMedia()) {
+			ol.add(new ObservationMediaEntry(om));
+		}
+		return ol;
+	}
+
+}
