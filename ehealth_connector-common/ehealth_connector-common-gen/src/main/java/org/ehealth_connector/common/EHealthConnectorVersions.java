ad3e138140517c62964c2195e98293611c842bdb
Tony Schaller <tony.schaller@medshare.net>
2016-05-13 12:50:05 +0000

    Prepared OID for next version
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1158 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-common/ehealth_connector-common-gen/src/main/java/org/ehealth_connector/common/EHealthConnectorVersions.java
+++ ehealth_connector-common/ehealth_connector-common-gen/src/main/java/org/ehealth_connector/common/EHealthConnectorVersions.java
@@ -45,7 +45,12 @@ public enum EHealthConnectorVersions {
 	 * OID: 2.16.756.5.30.1.139.1.1.5 eHealthConnector R201604
 	 */
 	EHealthConnectorR201604("2.16.756.5.30.1.139.1.1.5", "eHealthConnector R201604", "R20160430");
-
+	
+	/**
+	 * OID: 2.16.756.5.30.1.139.1.1.6 eHealthConnector R201611
+	 */
+	EHealthConnectorR201611("2.16.756.5.30.1.139.1.1.6", "eHealthConnector R201611", "R201611xx");
+	
 	public static EHealthConnectorVersions getCurrentVersion() {
 		return EHealthConnectorDev;
 	}
