eeb192cf2fb12560b87456e7c432d89bcde55ce1
Tony Schaller <tony.schaller@medshare.net>
2016-12-12 14:42:37 +0000

    Added FileUtil class
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1202 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

---
+++ ehealth_connector-common/ehealth_connector-common-gen/src/main/java/org/ehealth_connector/common/utils/FileUtil.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ *
+ * The authorship of this code and the accompanying materials is held by medshare GmbH, Switzerland.
+ * All rights reserved. http://medshare.net
+ *
+ * Project Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/
+ *
+ * This code is are made available under the terms of the Eclipse Public License v1.0.
+ *
+ * Accompanying materials are made available under the terms of the Creative Commons
+ * Attribution-ShareAlike 4.0 License.
+ *
+ * Year of publication: 2016
+ *
+ *******************************************************************************/
+package org.ehealth_connector.common.utils;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class FileUtil {
+
+	public static void saveString2File(String outputString, String outputFileName) {
+		Writer out = null;
+		try {
+			out = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream(outputFileName), "UTF-8"));
+			out.write(outputString);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+
+}
