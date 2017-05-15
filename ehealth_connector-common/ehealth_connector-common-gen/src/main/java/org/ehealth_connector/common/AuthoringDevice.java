37965a7ba2a9814ead5e38ddb10dc687ec8bbdb8
Stephane Spahni <Stephane.Spahni@hcuge>
2017-05-15 13:04:32 +0000

    Methods for accessing the Manufacturer Name
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1333 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-common/ehealth_connector-common-gen/src/main/java/org/ehealth_connector/common/AuthoringDevice.java
+++ ehealth_connector-common/ehealth_connector-common-gen/src/main/java/org/ehealth_connector/common/AuthoringDevice.java
@@ -2,18 +2,16 @@
  * The authorship of this project and accompanying materials is held by medshare GmbH, Switzerland.
  * All rights reserved. https://medshare.net
  *
- * Source code, documentation and other resources have been contributed by various people.
  * Project Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/
- * For exact developer information, please refer to the commit history of the forge.
  *
- * This code is made available under the terms of the Eclipse Public License v1.0.
+ * This code is are made available under the terms of the Eclipse Public License v1.0.
  *
  * Accompanying materials are made available under the terms of the Creative Commons
  * Attribution-ShareAlike 4.0 License.
  *
- * This line is intended for UTF-8 encoding checks, do not modify/delete: äöüéè
+ * Year of publication: 2016
  *
- */
+ *******************************************************************************/
 package org.ehealth_connector.common;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
@@ -77,6 +75,19 @@ public class AuthoringDevice {
 	}
 
 	/**
+	 * Gets the manufacturer name.
+	 *
+	 * @return the manufacturer name
+	 */
+	public String getManufacturerName() {
+		if ((mAuthoringDevice.getManufacturerModelName() != null)
+				&& (mAuthoringDevice.getManufacturerModelName().getText() != null)) {
+			return mAuthoringDevice.getManufacturerModelName().getText();
+		}
+		return null;
+	}
+
+	/**
 	 * Gets the software name.
 	 *
 	 * @return the software name
@@ -87,6 +98,18 @@ public class AuthoringDevice {
 			return mAuthoringDevice.getSoftwareName().getText();
 		}
 		return null;
+	}
+
+	/**
+	 * Sets the manufacturer name.
+	 *
+	 * @param name
+	 *            the new manufacturer name
+	 */
+	public void setManufacturerName(String name) {
+		final SC sc = DatatypesFactory.eINSTANCE.createSC();
+		sc.addText(name);
+		mAuthoringDevice.setManufacturerModelName(sc);
 	}
 
 	/**
