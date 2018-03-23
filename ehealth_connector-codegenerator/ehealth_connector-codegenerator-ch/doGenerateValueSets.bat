3db9ba5606ddc74623be5b796ad30ecea664e2e3
Thomas Weber <Thomas.Weber@SwissQ.it>
2018-03-23 17:06:55 +0000

    Fixed batch execution
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1457 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-codegenerator/ehealth_connector-codegenerator-ch/doGenerateValueSets.bat
+++ ehealth_connector-codegenerator/ehealth_connector-codegenerator-ch/doGenerateValueSets.bat
@@ -1,2 +1,3 @@
 @echo off
-java -cp target\codegenerator-ch.jar org.ehealth_connector.codegenerator.ch.valuesets.UpdateValueSets
+
+mvn exec:java
--- ehealth_connector-codegenerator/ehealth_connector-codegenerator-ch/doGenerateValueSets.sh
+++ ehealth_connector-codegenerator/ehealth_connector-codegenerator-ch/doGenerateValueSets.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# java -cp target/codegenerator-ch.jar org.ehealth_connector.codegenerator.ch.valuesets.UpdateValueSets
 mvn exec:java
