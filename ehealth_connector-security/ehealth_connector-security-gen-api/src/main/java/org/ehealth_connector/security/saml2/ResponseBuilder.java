a26e48516b0fa85a42105cc7b56825fa6575d882
Roeland Luykx <luykx@arpage.ch>
2018-03-29 12:32:09 +0000

    fix the false formated file headers
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1498 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-security/ehealth_connector-security-gen-api/src/main/java/org/ehealth_connector/security/saml2/ResponseBuilder.java
+++ ehealth_connector-security/ehealth_connector-security-gen-api/src/main/java/org/ehealth_connector/security/saml2/ResponseBuilder.java
@@ -1,16 +1,42 @@
 /*
- * The authorship of this project and accompanying materials is held by medshare GmbH, Switzerland. All rights reserved.
- * https://medshare.net Source code, documentation and other resources have been contributed by various people. Project Team:
- * https://sourceforge.net/p/ehealthconnector/wiki/Team/ For exact developer information, please refer to the commit history of the forge.
- * This code is made available under the terms of the Eclipse Public License v1.0. Accompanying materials are made available under the terms
- * of the Creative Commons Attribution-ShareAlike 4.0 License. This line is intended for UTF-8 encoding checks, do not modify/delete: äöüéè
+ *
+ * The authorship of this project and accompanying materials is held by medshare GmbH, Switzerland.
+ * All rights reserved. https://medshare.net
+ *
+ * Source code, documentation and other resources have been contributed by various people.
+ * Project Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/
+ * For exact developer information, please refer to the commit history of the forge.
+ *
+ * This code is made available under the terms of the Eclipse Public License v1.0.
+ *
+ * Accompanying materials are made available under the terms of the Creative Commons
+ * Attribution-ShareAlike 4.0 License.
+ *
+ * This line is intended for UTF-8 encoding checks, do not modify/delete: äöüéè
+ *
  */
 package org.ehealth_connector.security.saml2;
 
 /**
- * @since Feb 21, 2018 3:49:45 PM
- *
+ * <!-- @formatter:off -->
+ * <div class="en">Interface describing the methods of the ResponseBuilder.</div>
+ * <div class="de">Interface beschreibt die Methoden des ResponseBuilder.</div>
+ * <div class="fr">VOICIFRANCAIS</div>
+ * <div class="it">ITALIANO</div>
+ * <!-- @formatter:on -->
  */
 public interface ResponseBuilder extends SimpleBuilder<Response> {
+
+	// ResponseBuilder inResponseTo();
+	//
+	// ResponseBuilder destination(String aDestination);
+	//
+	// ResponseBuilder consent(String aConsent);
+	//
+	// ResponseBuilder status(Status aStatus);
+	//
+	// ResponseBuilder assertions(List<Assertion> aAssertions);
+	//
+	// ResponseBuilder encryptedAssertions(List<EncryptedAssertion> aEncryptedAssertions);
 
 }
