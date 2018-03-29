807b32029db28bece7b6e3d70658e335c6dcb580
Roeland Luykx <luykx@arpage.ch>
2018-03-29 12:31:53 +0000

    Complete javadoc
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1494 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-security/ehealth_connector-security-gen-api/src/main/java/org/ehealth_connector/security/deserialization/Deserializer.java
+++ ehealth_connector-security/ehealth_connector-security-gen-api/src/main/java/org/ehealth_connector/security/deserialization/Deserializer.java
@@ -11,15 +11,94 @@ import org.ehealth_connector.security.exceptions.DeserializeException;
 import org.w3c.dom.Element;
 
 /**
- * @since Feb 21, 2018 10:24:01 AM
- *
+ * <!-- @formatter:off -->
+ * <div class="en">Typed interface describing the methods for deserializing elements.</div>
+ * <div class="de">Interfacte welches die methoden für das deserialisieren von elemente gebraucht werden.</div>
+ * <div class="fr">VOICIFRANCAIS</div>
+ * <div class="it">ITALIANO</div>
+ * <!-- @formatter:on -->
  */
 public interface Deserializer<T> {
 
+	/**
+	 * 
+	 * <!-- @formatter:off -->
+	 * <div class="en">Method to deserialize from an xml Element to the to T corresponding Model.</div>
+	 * <div class="de">Methode um von einem xml Element zu einer dem T entsprechenden Model zu deserialisieren.</div>
+	 * <div class="fr">VOICIFRANCAIS</div>
+	 * <div class="it">ITALIANO</div>
+	 *
+	 * @param aXmlElement
+	 *  <div class="en">Method to deserialize the xml Element</div>
+	 *  <div class="de">Methode um das xml Element zu deserialisieren</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @return
+	 * 	<div class="en">the type T deserialized from xml Element</div>
+	 *  <div class="de">Der typ T deserialisiert vom xml Element</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @throws DeserializeException
+	 * 	<div class="en">will be thrown when an error occures on deserialization</div>
+	 *  <div class="de">wird geworfen wenn eine fehler deserialisieren auftritt</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * <!-- @formatter:on -->
+	 */
 	T fromXmlElement(Element aXmlElement) throws DeserializeException;
 
+	/**
+	 * 
+	 * <!-- @formatter:off -->
+	 * <div class="en">Method to deserialize from an xml String to the to T corresponding Model.</div>
+	 * <div class="de">Methode um von einem xml String zu einer dem T entsprechenden Model zu deserialisieren.</div>
+	 * <div class="fr">VOICIFRANCAIS</div>
+	 * <div class="it">ITALIANO</div>
+	 *
+	 * @param aXmlElement
+	 *  <div class="en">Method to deserialize the xml String</div>
+	 *  <div class="de">Methode um das xml String zu deserialisieren</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @return
+	 * 	<div class="en">the type T deserialized from xml String</div>
+	 *  <div class="de">Der typ T deserialisiert vom xml String</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @throws DeserializeException
+	 * 	<div class="en">will be thrown when an error occures on deserialization</div>
+	 *  <div class="de">wird geworfen wenn eine fehler deserialisieren auftritt</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * <!-- @formatter:on -->
+	 */
 	T fromXmlString(String aXmlString) throws DeserializeException;
 
+	/**
+	 * 
+	 * <!-- @formatter:off -->
+	 * <div class="en">Method to deserialize from an xml String to the to T corresponding Model.</div>
+	 * <div class="de">Methode um von einem xml String zu einer dem T entsprechenden Model zu deserialisieren.</div>
+	 * <div class="fr">VOICIFRANCAIS</div>
+	 * <div class="it">ITALIANO</div>
+	 *
+	 * @param aByteArray
+	 * <div class="en">Method to deserialize the xml byte array</div>
+	 *  <div class="de">Methode um das xml byte array zu deserialisieren</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @return
+	 * 	<div class="en">the type T deserialized from xml byte array</div>
+	 *  <div class="de">Der typ T deserialisiert vom xml byte array</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @throws DeserializeException
+	 * 	<div class="en">will be thrown when an error occures on deserialization</div>
+	 *  <div class="de">wird geworfen wenn eine fehler deserialisieren auftritt</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * <!-- @formatter:on -->
+	 */
 	T fromXmlByteArray(byte[] aByteArray) throws DeserializeException;
 
 }
