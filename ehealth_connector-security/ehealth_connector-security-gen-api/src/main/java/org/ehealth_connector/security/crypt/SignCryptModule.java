35932eccb60bed83b7000cb33e7a8d56f15ebe1b
Roeland Luykx <luykx@arpage.ch>
2018-03-29 12:31:51 +0000

    Complete javadoc
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1493 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

--- ehealth_connector-security/ehealth_connector-security-gen-api/src/main/java/org/ehealth_connector/security/crypt/SignCryptModule.java
+++ ehealth_connector-security/ehealth_connector-security-gen-api/src/main/java/org/ehealth_connector/security/crypt/SignCryptModule.java
@@ -1,166 +1,86 @@
 /*
  * The authorship of this project and accompanying materials is held by medshare GmbH, Switzerland. All rights reserved.
- * https://medshare.net Source code, documentation and other resources have been contributed by various people. Project
- * Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/ For exact developer information, please refer to the
- * commit history of the forge. This code is made available under the terms of the Eclipse Public License v1.0.
- * Accompanying materials are made available under the terms of the Creative Commons Attribution-ShareAlike 4.0 License.
- * This line is intended for UTF-8 encoding checks, do not modify/delete: äöüéè
+ * https://medshare.net Source code, documentation and other resources have been contributed by various people. Project Team:
+ * https://sourceforge.net/p/ehealthconnector/wiki/Team/ For exact developer information, please refer to the commit history of the forge.
+ * This code is made available under the terms of the Eclipse Public License v1.0. Accompanying materials are made available under the terms
+ * of the Creative Commons Attribution-ShareAlike 4.0 License. This line is intended for UTF-8 encoding checks, do not modify/delete: äöüéè
  */
 package org.ehealth_connector.security.crypt;
 
 import java.security.KeyStore;
 
-import org.ehealth_connector.security.saml2.Assertion;
-import org.ehealth_connector.security.saml2.Response;
+import org.ehealth_connector.security.authentication.AuthnRequest;
+import org.ehealth_connector.security.exceptions.SigningException;
 
 /**
- * @since Jan 16, 2018 2:20:16 PM
+ * 
+ * <!-- @formatter:off -->
+ * <div class="en">Interface describing the methods of the sign and crypt module.</div>
+ * <div class="de">Interfacte welches die methoden des sign und crypt modules beschreiben.</div>
+ * <div class="fr">VOICIFRANCAIS</div>
+ * <div class="it">ITALIANO</div>
+ * <!-- @formatter:on -->
  *
  */
 public interface SignCryptModule {
+
 	/**
 	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Setzt die Referenz zum Key Store mit den Schlüsseln zur Signierung.</div>
+	 * <!-- @formatter:off -->
+	 * <div class="en">Method to set the key and truststore and the corresponding passwords.</div>
+	 * <div class="de">Methode um den key und trust store und deren passwörter zu setzen.</div>
 	 * <div class="fr">VOICIFRANCAIS</div>
 	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
 	 *
 	 * @param keyStore
+	 *  <div class="en">the keystore.</div>
+	 *  <div class="de">der Keystore der gesetzt werde soll.</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @param aKeyStorePassword
+	 * 	<div class="en">the password of the keystore to be set.</div>
+	 *  <div class="de">das passwort zum Keystore</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
 	 * @param trustStore
+	 *  <div class="en">the truststore to be set.</div>
+	 *  <div class="de">der Truststore der gesetzt werde soll.</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @param aTrustStorePassword
+	 *  <div class="en">the password of the truststore to be set.</div>
+	 *  <div class="de">das passwort zum Truststore</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * <!-- @formatter:on -->
 	 */
-	public void setPki(KeyStore keyStore, KeyStore trustStore);
+	void setPki(KeyStore keyStore, String aKeyStorePassword, KeyStore trustStore, String aTrustStorePassword);
 
 	/**
 	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Entschlüsselt einen SAML Response, der als XML-String übergeben wird.</div>
+	 * <!-- @formatter:off -->
+	 * <div class="en">Method to sign an AuthnRequest.</div>
+	 * <div class="de">Methode um einen AuthnRequest zu signieren.</div>
 	 * <div class="fr">VOICIFRANCAIS</div>
 	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
+	 * <!-- @formatter:on -->
 	 *
-	 * @param samlResponse
-	 * @return
+	 * @param aAuthnRequest
+	 * <div class="en">the AuthnRequest to be signed</div>
+	 *  <div class="de">der AuthnRequest der signiert werden soll</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @param aSigningAlias
+	 * <div class="en">the alias of the key in the keystore to be used for signing</div>
+	 *  <div class="de">der Alias des Keys aus dem Keystore welcher zum signieren gebraucht werden soll</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
+	 * @throws SigningException
+	 * <div class="en">will be thrown when an error occures signing the AuthnRequest</div>
+	 *  <div class="de">wird geworfen wenn eine fehler beim Signieren des Authnrequests auftritt</div>
+	 *  <div class="fr">VOICIFRANCAIS</div>
+	 *  <div class="it">ITALIANO</div>
 	 */
-	public Response decryptFromStringResponse(String samlResponse);
+	void signAuthnRequest(AuthnRequest aAuthnRequest, String aSigningAlias) throws SigningException;
 
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Entschlüsselt einen SAML Response, der als XML-Objekt übergeben wird.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param samlResponse
-	 * @return
-	 */
-	public Response decryptFromXmlResponse(org.w3c.dom.Element samlResponse);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Entschlüsselt eine SAML Assertion, die als XML-String übergeben wird.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param samlAssertion
-	 * @return
-	 */
-	public Assertion decryptFromStringAssertion(String samlAssertion);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Entschlüsselt eine SAML Assertion, die als XML-Objekt übergeben wird.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param samlAssertion
-	 * @return
-	 */
-	public Assertion decryptFromXmlAssertion(org.w3c.dom.Element samlAssertion);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Verschlüsselt eine SAML Assertion.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param assertion
-	 * @param trustAlias
-	 * @return
-	 */
-	public org.w3c.dom.Element encryptAssertionToXml(Assertion assertion, String trustAlias);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Verschlüsselt einen SAML Response.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param response
-	 * @param trustAlias
-	 * @return
-	 */
-	public org.w3c.dom.Element encryptResponseToXml(Response response, String trustAlias);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Signiert eine SAML Assertion.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param assertion
-	 * @param keyAlias
-	 * @return
-	 */
-	public org.w3c.dom.Element signAssertionToXml(Assertion assertion, String keyAlias);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Verschlüsselt und signiert eine SAML Assertion.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param assertion
-	 * @param trustAlias
-	 * @param keyAlias
-	 * @return
-	 */
-	public org.w3c.dom.Element encryptAndSign(Assertion assertion, String trustAlias,
-			String keyAlias);
-
-	/**
-	 * 
-	 * @formatter:off
-	 * <div class="en">HEREISENGLISH</div>
-	 * <div class="de">Wandelt das Element (z.B. Assertion oder Response) vom XML Element in einen String um.</div>
-	 * <div class="fr">VOICIFRANCAIS</div>
-	 * <div class="it">ITALIANO</div>
-	 * @formatter:on
-	 *
-	 * @param element
-	 * @return
-	 */
-	public String toString(org.w3c.dom.Element element);
 }
