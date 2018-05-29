9ef75b29553df3afac72b063d7b7e21be22be385
Roeland Luykx <luykx@arpage.ch>
2018-05-29 07:20:40 +0000

    Add atna stuff
    git-svn-id: https://svn.code.sf.net/p/ehealthconnector/code/trunk@1592 e99adb7c-5e35-42fd-8d1b-cdb378af05e8

---
+++ ehealth_connector-security/ehealth_connector-security-ch-impl/src/main/java/org/ehealth_connector/security/ch/ppq/atna/PPQConsumerAuditor.java
@@ -0,0 +1,33 @@
+/*
+ *
+ * The authorship of this project and accompanying materials is held by medshare GmbH, Switzerland.
+ * All rights reserved. https://medshare.net
+ *
+ * Source code, documentation and other resources have been contributed by various people.
+ * Project Team: https://sourceforge.net/p/ehealthconnector/wiki/Team/
+ * For exact developer information, please refer to the commit history of the forge.
+ *
+ * This code is made available under the terms of the Eclipse Public License v1.0.
+ *
+ * Accompanying materials are made available under the terms of the Creative Commons
+ * Attribution-ShareAlike 4.0 License.
+ *
+ * This line is intended for UTF-8 encoding checks, do not modify/delete: äöüéè
+ *
+ */
+package org.ehealth_connector.security.ch.ppq.atna;
+
+import org.openhealthtools.ihe.atna.auditor.IHEAuditor;
+
+/**
+ * <!-- @formatter:off -->
+ * <div class="en">Class implementing the PPQ Consumer Auditor for ATNA</div>
+ * <div class="de">Klasse die den PPQ Consumer Auditor für ATNA implementiert</div>
+ * <div class="fr">VOICIFRANCAIS</div>
+ * <div class="it">ITALIANO</div>
+ * 
+ * <!-- @formatter:on -->
+ */
+public class PPQConsumerAuditor extends IHEAuditor {
+
+}
