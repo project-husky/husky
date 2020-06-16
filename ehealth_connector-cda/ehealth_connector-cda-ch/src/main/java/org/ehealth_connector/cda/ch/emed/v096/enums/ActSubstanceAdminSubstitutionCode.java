595698e6d9aac53f2f924fe4015f0f54edd1e414
tschaller <e-Health.2019>
2020-06-16 23:04:12 +0200

    Revert Updated Enums after new ART-DECOR 2 Java Generator Run for CDA-CH-EMED; due to manually added translations. Shall be fixed in a future commit

--- ehealth_connector-cda/ehealth_connector-cda-ch/src/main/java/org/ehealth_connector/cda/ch/emed/v096/enums/ActSubstanceAdminSubstitutionCode.java
+++ ehealth_connector-cda/ehealth_connector-cda-ch/src/main/java/org/ehealth_connector/cda/ch/emed/v096/enums/ActSubstanceAdminSubstitutionCode.java
@@ -16,18 +16,20 @@
  */
 package org.ehealth_connector.cda.ch.emed.v096.enums;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.Generated;
-
 import org.ehealth_connector.common.enums.CodeSystems;
 import org.ehealth_connector.common.enums.LanguageCode;
 import org.ehealth_connector.common.mdht.enums.ValueSetEnumInterface;
 
+import javax.annotation.Generated;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <!-- @formatter:off -->
- * <div class="en"><p>    <b>History description 2014-03-26: </b>Lock all vaue sets untouched since 2014-03-26 to trackingId 2014T1_2014_03_26</p></div>
+ * <div class="en"><p>Note this class was updated manually to add the missings translation, if we regenerate with the art decor generator thoses modification will be missing</p>
+ * <p>
+ *     <b>History description 2014-03-26: </b>Lock all vaue sets untouched since 2014-03-26 to trackingId 2014T1_2014_03_26
+ * </p></div>
  * <div class="de">no designation found for language GERMAN</div>
  * <div class="fr">no designation found for language FRENCH</div>
  * <div class="it">no designation found for language ITALIAN</div>
@@ -39,10 +41,101 @@ public enum ActSubstanceAdminSubstitutionCode implements ValueSetEnumInterface {
 	/**
 	 * <!-- @formatter:off -->
 	 * <div class="en">equivalent</div>
+	 * <div class="de">Äquivalent</div>
+	 * <div class="fr">équivalent</div>
+	 * <div class="it">equivalente</div>
 	 * <!-- @formatter:on -->
 	 */
-	EQUIVALENT("E", "2.16.840.1.113883.5.1070", "equivalent", "equivalent", "TOTRANSLATE",
-			"TOTRANSLATE", "TOTRANSLATE");
+	EQUIVALENT("E", "2.16.840.1.113883.5.139", "equivalent",
+			"equivalent", "Äquivalent", "équivalent", "equivalente"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">equivalent composition</div>
+	 * <div class="de">ähnliche Zusammensetzung</div>
+	 * <div class="fr">composition équivalente</div>
+	 * <div class="it">composizione simile</div>
+	 * <!-- @formatter:on -->
+	 */
+	EQUIVALENT_COMPOSITION("EC", "2.16.840.1.113883.5.139", "equivalent composition",
+			"equivalent composition", "ähnliche Zusammensetzung", "composition équivalente", "composizione simile"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">brand composition</div>
+	 * <div class="de">Äquivalent desselben Herstellers</div>
+	 * <div class="fr">équivalent même fabriquant</div>
+	 * <div class="it">equivalente dello stesso produttore</div>
+	 * <!-- @formatter:on -->
+	 */
+	BRAND_COMPOSITION("BC", "2.16.840.1.113883.5.139", "brand composition",
+			"brand composition", "Äquivalent desselben Herstellers", "équivalent même fabriquant", "equivalente dello stesso produttore"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">generic composition</div>
+	 * <div class="de">identische Zusammensetzung</div>
+	 * <div class="fr">composition identique</div>
+	 * <div class="it">composizione identica</div>
+	 * <!-- @formatter:on -->
+	 */
+	GENERIC_COMPOSITION("G", "2.16.840.1.113883.5.139", "generic composition",
+			"generic composition", "identische Zusammensetzung", "composition identique", "composizione identica"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">therapeutic alternative</div>
+	 * <div class="de">therapeutische Alternative</div>
+	 * <div class="fr">alternative thérapeutique</div>
+	 * <div class="it">alternativa terapeutica</div>
+	 * <!-- @formatter:on -->
+	 */
+	THERAPEUTIC_ALTERNATIVE("TE", "2.16.840.1.113883.5.139", "therapeutic alternative",
+			"therapeutic alternative", "therapeutische Alternative", "alternative thérapeutique", "alternativa terapeutica"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">therapeutic brand</div>
+	 * <div class="de">therapeutische Alternative gleicher Hersteller</div>
+	 * <div class="fr">alternative thérapeutique même fabricant</div>
+	 * <div class="it">alternativa terapeutica stesso produttore</div>
+	 * <!-- @formatter:on -->
+	 */
+	THERAPEUTIC_BRAND("TB", "2.16.840.1.113883.5.139", "therapeutic brand",
+			"therapeutic brand", "therapeutische Alternative gleicher Hersteller", "alternative thérapeutique même fabricant", "alternativa terapeutica stesso produttore"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">therapeutic generic</div>
+	 * <div class="de">gleiche therapeutische Wirkung</div>
+	 * <div class="fr">effet thérapeutique identique</div>
+	 * <div class="it">stesso effetto terapeutico</div>
+	 * <!-- @formatter:on -->
+	 */
+	THERAPEUTIC_GENERIC("TG", "2.16.840.1.113883.5.139", "therapeutic generic",
+			"therapeutic generic", "gleiche therapeutische Wirkung", "effet thérapeutique identique", "stesso effetto terapeutico"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">formulary</div>
+	 * <div class="de">dieselbe Formel</div>
+	 * <div class="fr">même formule</div>
+	 * <div class="it">stessa formula</div>
+	 * <!-- @formatter:on -->
+	 */
+	FORMULARY("F", "2.16.840.1.113883.5.139", "formulary",
+			"formulary", "dieselbe Formel", "même formule", "stessa formula"),
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">none</div>
+	 * <div class="de">nicht autorisiert</div>
+	 * <div class="fr">non autorisé</div>
+	 * <div class="it">non autorizzato</div>
+	 * <!-- @formatter:on -->
+	 */
+	NONE("N", "2.16.840.1.113883.5.139", "none",
+			"none", "nicht autorisiert", "non autorisé", "non autorizzato");
 
 	/**
 	 * <!-- @formatter:off -->
@@ -50,6 +143,64 @@ public enum ActSubstanceAdminSubstitutionCode implements ValueSetEnumInterface {
 	 * <!-- @formatter:on -->
 	 */
 	public static final String EQUIVALENT_CODE = "E";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for equivalent composition</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String EQUIVALENT_COMPOSITION_CODE = "EC";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for brand composition</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String BRAND_COMPOSITION_CODE = "BC";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for generic composition</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String GENERIC_COMPOSITION_CODE = "G";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for therapeutic alternative</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String THERAPEUTIC_ALTERNATIVE_CODE = "TE";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for therapeutic brand</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String THERAPEUTIC_BRAND_CODE = "TB";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for therapeutic generic</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String THERAPEUTIC_GENERIC_CODE = "TG";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for formulary</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String FORMULARY_CODE = "F";
+
+	/**
+	 * <!-- @formatter:off -->
+	 * <div class="en">Code for none</div>
+	 * <!-- @formatter:on -->
+	 */
+	public static final String NONE_CODE = "N";
+
+
 
 	/**
 	 * <!-- @formatter:off -->
@@ -170,8 +321,8 @@ public enum ActSubstanceAdminSubstitutionCode implements ValueSetEnumInterface {
 	 *            the display name it
 	 */
 	ActSubstanceAdminSubstitutionCode(String code, String codeSystem, String displayName,
-			String displayNameEn, String displayNameDe, String displayNameFr,
-			String displayNameIt) {
+                                      String displayNameEn, String displayNameDe, String displayNameFr,
+                                      String displayNameIt) {
 		this.code = code;
 		this.codeSystem = codeSystem;
 		displayNames = new HashMap<>();
