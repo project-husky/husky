5c1f2ac12ba1a5a02f99ddec8e3430db5eb434d8
Anna <anna.jungwirth@gmx.at>
2021-11-17 10:43:12 +0100

    adapted header of every java class according to Carole's information

--- husky-common/husky-common-gen/src/main/java/org/husky/common/hl7cdar2/ParticipationHolder.java
+++ husky-common/husky-common-gen/src/main/java/org/husky/common/hl7cdar2/ParticipationHolder.java
@@ -1,5 +1,6 @@
 /*
- * The authorship of this project and accompanying materials is held by medshare GmbH, Switzerland.
+ * This code is made available under the terms of the Eclipse Public License v1.0 in the github project https://github.com/project-husky/husky there you also find a list of the contributors and the license information.
+This project has been developed further and modified by the joined working group Husky on the basis of the eHealth Connector opensource project from June 28, 2021, whereas medshare GmbH is the initial and main contributor/author of the eHealth Connector.
  * All rights reserved. https://medshare.net
  *
  * Source code, documentation and other resources have been contributed by various people.
