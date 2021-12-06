7f74470c334466e9145d3522bee94a805ba5fa61
Quentin Ligier <quentin.ligier@bluewin.ch>
2021-12-06 17:03:13 +0100

    Create module husky-emed-narrative

--- husky-emed/husky-emed-narrative/src/main/java/org/husky/emed/narrative/StrucDocTextSectionNarrativeGenerator.java
+++ husky-emed/husky-emed-narrative/src/main/java/org/husky/emed/narrative/StrucDocTextSectionNarrativeGenerator.java
@@ -1,4 +1,13 @@
-package org.husky.emed.cda.services.narrative;
+/*
+ * This code is made available under the terms of the Eclipse Public License v1.0
+ * in the github project https://github.com/project-husky/husky there you also
+ * find a list of the contributors and the license information.
+ *
+ * This project has been developed further and modified by the joined working group Husky
+ * on the basis of the eHealth Connector opensource project from June 28, 2021,
+ * whereas medshare GmbH is the initial and main contributor/author of the eHealth Connector.
+ */
+package org.husky.emed.narrative;
 
 import org.husky.emed.cda.models.document.EmedDisDocumentDigest;
 import org.husky.emed.cda.models.document.EmedMtpDocumentDigest;
