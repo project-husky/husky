be40eb1ec0e3e869dadb0d2882b89dc055978fc1
Anna Jungwirth <anna.jungwirth@arztis.at>
2022-01-05 15:56:48 +0100

    fixed compiler error

--- husky-communication/husky-communication-ch/src/test/java/org/husky/communication/ch/DocumentMetadataChTest.java
+++ husky-communication/husky-communication-ch/src/test/java/org/husky/communication/ch/DocumentMetadataChTest.java
@@ -16,60 +16,59 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.husky.common.ch.enums.ConfidentialityCode;
 import org.husky.common.enums.LanguageCode;
-import org.husky.communication.DocumentMetadataTest;
-import org.husky.communication.ch.DocumentMetadataCh;
 import org.husky.communication.ch.enums.ClassCode;
 import org.husky.communication.ch.enums.HealthcareFacilityTypeCode;
 import org.husky.communication.ch.enums.MimeType;
 import org.husky.communication.ch.enums.PracticeSettingCode;
 import org.husky.communication.ch.enums.TypeCode;
+import org.husky.communication.testhelper.TestUtils;
 import org.junit.jupiter.api.Test;
 
-public class DocumentMetadataChTest extends DocumentMetadataTest {
+class DocumentMetadataChTest extends TestUtils {
 
 	DocumentMetadataCh m = new DocumentMetadataCh();
 
 	@Test
-	public void testClassCodeEnum() {
+	void testClassCodeEnum() {
 		m.setClassCode(ClassCode.CARE_PLAN);
 		assertTrue(isEqual(ClassCode.CARE_PLAN.getCode(), m.getClassCodeEnum().getCode()));
 	}
 
 	@Test
-	public void testCodedLanguageEnum() {
+	void testCodedLanguageEnum() {
 		m.setCodedLanguage(LanguageCode.FRENCH);
 		assertEquals(LanguageCode.FRENCH, m.getLanguageCodeEnum());
 	}
 
 	@Test
-	public void testConfidentialityCodeEnum() {
+	void testConfidentialityCodeEnum() {
 		m.addConfidentialityCode(ConfidentialityCode.NORMALLY_ACCESSIBLE);
 		assertTrue(isEqual(ConfidentialityCode.NORMALLY_ACCESSIBLE.getCode(),
 				m.getConfidentialityCodesEnum().get(0).getCode()));
 	}
 
 	@Test
-	public void testHealthcareFacilityEnum() {
+	void testHealthcareFacilityEnum() {
 		m.setHealthcareFacilityTypeCode(HealthcareFacilityTypeCode.AMBULATORY_CARE_SITE);
 		assertEquals(HealthcareFacilityTypeCode.AMBULATORY_CARE_SITE,
 				m.getHealthcareFacilityTypeCodeEnum());
 	}
 
 	@Test
-	public void testMimeTypeEnum() {
+	void testMimeTypeEnum() {
 		m.setMimeType(MimeType.CDA_LEVEL_1_MULTIPART);
 		assertEquals(MimeType.CDA_LEVEL_1_MULTIPART.getCodeValue(), m.getMimeType());
 	}
 
 	@Test
-	public void testPracticeSettingEnum() {
+	void testPracticeSettingEnum() {
 		m.setPracticeSettingCode(PracticeSettingCode.CLINICAL_IMMUNOLOGY_ALLERGY);
 		assertEquals(PracticeSettingCode.CLINICAL_IMMUNOLOGY_ALLERGY,
 				m.getPracticeSettingCodeEnum());
 	}
 
 	@Test
-	public void testTypeCodeEnum() {
+	void testTypeCodeEnum() {
 		m.setTypeCode(TypeCode.DISCHARGE_SUMMARY);
 		assertEquals(TypeCode.DISCHARGE_SUMMARY, m.getTypeCodeEnum());
 	}
