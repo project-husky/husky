1213ac3c59004652baa36654775e1a0cf8a3e5c2
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-04-25 17:46:37 +0200

    CH:PHARM-1: Fix comment

--- husky-communication/husky-communication-ch/src/main/java/org/husky/communication/ch/camel/chpharm1/package-info.java
+++ husky-communication/husky-communication-ch/src/main/java/org/husky/communication/ch/camel/chpharm1/package-info.java
@@ -11,12 +11,8 @@
 /**
  * This is the definition of the CH:PHARM-1 transaction, an extension of IHE PHARM-1.
  *
- * <p>The Camel components are not redefined, those from the PHARM-1 transaction shall be used. The response models are
- * not redefined either, only the query models.
+ * <p>The response models are not redefined, only the query models and the CAMEL component.
  *
- * @see org.openehealth.ipf.commons.ihe.xds.pharm1.Pharm1PortType
- * @see org.openehealth.ipf.platform.camel.ihe.xds.pharm1.Pharm1Service
- * @see org.openehealth.ipf.platform.camel.ihe.xds.pharm1.Pharm1Component
  * @see org.openehealth.ipf.commons.ihe.xds.core.responses.QueryResponse
  *
  * @author Quentin Ligier
