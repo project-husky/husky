a3171450e2cdde987d021d1472a5706ef99bb581
Martin Smock <martin_smock@me.com>
2022-09-04 12:51:23 +0200

    added X-User Assertion to Provide and Register transaction

--- husky-communication/husky-communication-gen/src/test/java/org/husky/communication/integration/CHProvideAndRegisterDocumentSetTest.java
+++ husky-communication/husky-communication-gen/src/test/java/org/husky/communication/integration/CHProvideAndRegisterDocumentSetTest.java
@@ -1,15 +1,16 @@
 /*
- * This code is made available under the terms of the Eclipse Public License v1.0 
- * in the github project https://github.com/project-husky/husky there you also 
+ * This code is made available under the terms of the Eclipse Public License v1.0
+ * in the github project https://github.com/project-husky/husky there you also
  * find a list of the contributors and the license information.
- * 
- * This project has been developed further and modified by the joined working group Husky 
- * on the basis of the eHealth Connector opensource project from June 28, 2021, 
+ *
+ * This project has been developed further and modified by the joined working group Husky
+ * on the basis of the eHealth Connector opensource project from June 28, 2021,
  * whereas medshare GmbH is the initial and main contributor/author of the eHealth Connector.
  *
  */
 package org.husky.communication.integration;
 
+import org.apache.commons.io.IOUtils;
 import org.husky.common.basetypes.NameBaseType;
 import org.husky.common.communication.AffinityDomain;
 import org.husky.common.communication.Destination;
@@ -25,13 +26,25 @@ import org.husky.common.model.Name;
 import org.husky.communication.ConvenienceCommunication;
 import org.husky.communication.testhelper.TestApplication;
 import org.husky.communication.testhelper.XdsTestUtils;
+import org.husky.xua.communication.clients.XuaClient;
+import org.husky.xua.communication.clients.impl.ClientFactory;
+import org.husky.xua.communication.config.XuaClientConfig;
+import org.husky.xua.communication.config.impl.XuaClientConfigBuilderImpl;
+import org.husky.xua.communication.xua.RequestType;
+import org.husky.xua.communication.xua.TokenType;
+import org.husky.xua.communication.xua.XUserAssertionResponse;
+import org.husky.xua.communication.xua.impl.AppliesToBuilderImpl;
+import org.husky.xua.communication.xua.impl.XUserAssertionRequestBuilderImpl;
+import org.husky.xua.deserialization.impl.AssertionDeserializerImpl;
+import org.husky.xua.hl7v3.PurposeOfUse;
+import org.husky.xua.hl7v3.Role;
+import org.husky.xua.hl7v3.impl.CodedWithEquivalentsBuilder;
 import org.husky.xua.saml2.Assertion;
 import org.husky.xua.saml2.impl.AssertionBuilderImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openehealth.ipf.commons.core.OidGenerator;
-import org.openehealth.ipf.commons.ihe.xacml20.stub.saml20.assertion.NameIDType;
 import org.openehealth.ipf.commons.ihe.xds.core.responses.*;
 import org.opensaml.core.config.InitializationService;
 import org.slf4j.Logger;
@@ -45,167 +58,215 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
-import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Test class to test the ProvideAndRegisterDocumentSet [ITI-41] transaction with Swiss metadata requirements.
+ * Test class to test the ProvideAndRegisterDocumentSet [ITI-41] transaction with Swiss metadata requirements. This
+ * test performs the following steps:
+ * 1. load a test IdP Assertion from the disk
+ * 2. Use the IdP Assertion in conjunction with the claims (role, purposeOfUse, EPR-SPID of the patient health record)
+ * and request a X-User Assertion.
+ * 3. Use the X-User Assertion in conjunction with a specific set of XDS metadata and the JSON file from disk in a
+ * ProvideAndRegisterDocumentSet [ITI-41] transaction.
  */
 @ExtendWith(value = SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = { TestApplication.class })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = {TestApplication.class})
 @EnableAutoConfiguration
 class CHProvideAndRegisterDocumentSetTest extends XdsTestUtils {
 
-	static final Logger LOGGER = LoggerFactory.getLogger(CHProvideAndRegisterDocumentSetTest.class.getName());
+    static final Logger LOGGER = LoggerFactory.getLogger(CHProvideAndRegisterDocumentSetTest.class.getName());
 
-	@Autowired
-	private ConvenienceCommunication convenienceCommunication;
+    @Autowired
+    private ConvenienceCommunication convenienceCommunication;
 
-	private AffinityDomain affinityDomain = null;
+    private AffinityDomain affinityDomain = null;
 
 
-	/**
-	 * Creates and start spring test application and set the endpoint of XDS Repository.
-	 *
-	 * @throws Exception if something unexpected happens
-	 */
-	@BeforeEach
-	public void setUp() throws Exception {
+    /**
+     * Creates and start spring test application and set the endpoint of XDS Repository.
+     *
+     * @throws Exception if something unexpected happens
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
 
-		InitializationService.initialize();
+        InitializationService.initialize();
 
-		// create and start spring test application
-		var app = new SpringApplication(TestApplication.class);
-		app.setWebApplicationType(WebApplicationType.NONE);
-		app.run();
+        // create and start spring test application
+        var app = new SpringApplication(TestApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run();
 
-		final Destination dest = new Destination();
+        final Destination dest = new Destination();
 
-		// set the URL of the community repository you want to store the document to
-		final String repositoryURL = "http://ehealthsuisse.ihe-europe.net:8280/xdstools7/sim/epr-testing__for_init_gw_testing/rep/prb";
+        // set the URL of the community repository you want to store the document to
+        final String repositoryURL = "http://ehealthsuisse.ihe-europe.net:8280/xdstools7/sim/epr-testing__for_init_gw_testing/rep/prb";
 
-		dest.setUri(new URI(repositoryURL));
+        dest.setUri(new URI(repositoryURL));
 
-		// add the OID of your application as assigned by the community
-		final String senderApplicationOid = "1.2.3.4";
-		dest.setSenderApplicationOid(senderApplicationOid);
+        // add the OID of your application as assigned by the community
+        final String senderApplicationOid = "1.2.3.4";
+        dest.setSenderApplicationOid(senderApplicationOid);
 
-		// add an application name
-		final String applicationName = "Clinical Information System"; //"2.16.840.1.113883.3.72.6.5.100.1399";
-		dest.setReceiverApplicationOid(applicationName);
+        // add an application name
+        final String applicationName = "Clinical Information System"; //"2.16.840.1.113883.3.72.6.5.100.1399";
+        dest.setReceiverApplicationOid(applicationName);
 
-		// add the name of your institution
-		final String facilityName = "Hospital in Bern";
-		dest.setReceiverFacilityOid(facilityName);
+        // add the name of your institution
+        final String facilityName = "Hospital in Bern";
+        dest.setReceiverFacilityOid(facilityName);
 
-		affinityDomain = new AffinityDomain();
-		affinityDomain.setRegistryDestination(dest);
-		affinityDomain.setRepositoryDestination(dest);
+        affinityDomain = new AffinityDomain();
+        affinityDomain.setRegistryDestination(dest);
+        affinityDomain.setRepositoryDestination(dest);
 
-		// remove cached documents in ConvenienceCommunication
-		convenienceCommunication.clearDocuments();
-	}
+        // remove cached documents in ConvenienceCommunication
+        convenienceCommunication.clearDocuments();
+    }
 
-	/**
-	 * This test checks the
-	 * {@link ConvenienceCommunication#submit(SubmissionSetMetadata, org.husky.xua.core.SecurityHeaderElement, String)}
-	 * with a JSON Document and Swiss compliant metadata
-	 * 
-	 * @throws Exception if something unexpected happens
-	 */
-	@Test
-	void submitJSONDocumentTest() throws Exception {
+    /**
+     * This tests the
+     * {@link ConvenienceCommunication#submit(SubmissionSetMetadata, org.husky.xua.core.SecurityHeaderElement, String)}
+     * with a JSON Document, Swiss compliant metadata and a X-User Assertion
+     *
+     * @throws Exception if something unexpected happens
+     */
+    @Test
+    void submitJSONDocumentTest() throws Exception {
 
-		convenienceCommunication.setAffinityDomain(affinityDomain);
-		convenienceCommunication.clearDocuments();
+        convenienceCommunication.setAffinityDomain(affinityDomain);
+        convenienceCommunication.clearDocuments();
 
-		// read and add file
-		final File file = new File("src/test/resources/docSource/FHIR-Vaccination.json");
-		final DocumentMetadata documentMetadata = convenienceCommunication.addDocument(DocumentDescriptor.FHIR_JSON, new FileInputStream(file));
+        // read and add file
+        final File file = new File("src/test/resources/docSource/FHIR-Vaccination.json");
+        final DocumentMetadata documentMetadata = convenienceCommunication.addDocument(DocumentDescriptor.FHIR_JSON, new FileInputStream(file));
 
-		documentMetadata.setTitle("Impfung");
+        documentMetadata.setTitle("Impfung");
 
-		// set the author data
-		final Name name = new Name(new NameBaseType());
-		name.setGiven("Peter");
-		name.setFamily("Müller");
-		name.setPrefix("Dr. med");
+        // set the author data
+        final Name name = new Name(new NameBaseType());
+        name.setGiven("Peter");
+        name.setFamily("Müller");
+        name.setPrefix("Dr. med");
 
-		final Author author = new Author();
-		author.addName(name);
+        final Author author = new Author();
+        author.addName(name);
 
-		final Code role = new Code("HCP", "2.16.756.5.30.1.127.3.10.6", "Healthcare professional");
-		author.setRoleFunction(role);
+        final Code role = new Code("HCP", "2.16.756.5.30.1.127.3.10.6", "Healthcare professional");
+        author.setRoleFunction(role);
 
-		documentMetadata.addAuthor(author);
+        documentMetadata.addAuthor(author);
 
-		final Identificator globalPatientId = new Identificator("1.3.6.1.4.1.21367.13.20.1000", "IHERED-1024");
-		documentMetadata.setDestinationPatientId(globalPatientId);
+        final Identificator globalPatientId = new Identificator("1.3.6.1.4.1.21367.13.20.1000", "IHERED-1024");
+        documentMetadata.setDestinationPatientId(globalPatientId);
 
-		final Identificator localPatientId = new Identificator("1.2.3.4", "2342134localid");
-		documentMetadata.setSourcePatientId(localPatientId);
+        final Identificator localPatientId = new Identificator("1.2.3.4", "2342134localid");
+        documentMetadata.setSourcePatientId(localPatientId);
 
-		documentMetadata.setCodedLanguage(LanguageCode.GERMAN_CODE);
+        documentMetadata.setCodedLanguage(LanguageCode.GERMAN_CODE);
 
 		/*
 		type code should be ("urn:che:epr:ch-vacd:immunization-administration:2022", "1.3.6.1.4.1.19376.1.2.3", "CH VACD Immunization Administration")
 		but this code is currently not supported by the test system
 		 */
-		final Code type = new Code("41000179103", "2.16.840.1.113883.6.96", "Immunization Record (record artifact)");
-		documentMetadata.setTypeCode(type);
+        final Code type = new Code("41000179103", "2.16.840.1.113883.6.96", "Immunization Record (record artifact)");
+        documentMetadata.setTypeCode(type);
 
-		final Code format = new Code("urn:che:epr:EPR_Unstructured_Document", "2.16.756.5.30.1.127.3.10.10",
-				"Unstructured EPR document");
-		documentMetadata.setFormatCode(format);
+        final Code format = new Code("urn:che:epr:EPR_Unstructured_Document", "2.16.756.5.30.1.127.3.10.10",
+                "Unstructured EPR document");
+        documentMetadata.setFormatCode(format);
 
-		final Code clazz = new Code("184216000", "2.16.840.1.113883.6.96", "Patient record type (record artifact)");
-		documentMetadata.setClassCode(clazz);
+        final Code clazz = new Code("184216000", "2.16.840.1.113883.6.96", "Patient record type (record artifact)");
+        documentMetadata.setClassCode(clazz);
 
-		final Code facility = new Code("22232009", "2.16.840.1.113883.6.96", "Hospital (environment)");
-		documentMetadata.setHealthcareFacilityTypeCode(facility);
+        final Code facility = new Code("22232009", "2.16.840.1.113883.6.96", "Hospital (environment)");
+        documentMetadata.setHealthcareFacilityTypeCode(facility);
 
-		final Code practice = new Code("394802001", "2.16.840.1.113883.6.96", "General medicine (qualifier value)");
-		documentMetadata.setPracticeSettingCode(practice);
+        final Code practice = new Code("394802001", "2.16.840.1.113883.6.96", "General medicine (qualifier value)");
+        documentMetadata.setPracticeSettingCode(practice);
 
-		final Code confidentiality = new Code("17621005", "2.16.840.1.113883.6.96", "Normal");
-		documentMetadata.addConfidentialityCode(confidentiality);
+        final Code confidentiality = new Code("17621005", "2.16.840.1.113883.6.96", "Normal");
+        documentMetadata.addConfidentialityCode(confidentiality);
 
 
-		// submission set metadata settings
-		final SubmissionSetMetadata submissionSetMetadata = new SubmissionSetMetadata();
-		submissionSetMetadata.setUniqueId(OidGenerator.uniqueOid().toString());
+        // submission set metadata settings
+        final SubmissionSetMetadata submissionSetMetadata = new SubmissionSetMetadata();
+        submissionSetMetadata.setUniqueId(OidGenerator.uniqueOid().toString());
 
-		submissionSetMetadata.setSourceId(EhcVersions.getCurrentVersion().getOid());
-		submissionSetMetadata.setEntryUUID(UUID.randomUUID().toString());
+        submissionSetMetadata.setSourceId(EhcVersions.getCurrentVersion().getOid());
+        submissionSetMetadata.setEntryUUID(UUID.randomUUID().toString());
 
-		submissionSetMetadata.setDestinationPatientId(globalPatientId);
+        submissionSetMetadata.setDestinationPatientId(globalPatientId);
 
-		submissionSetMetadata.getAuthor().add(authorPerson);
+        submissionSetMetadata.getAuthor().add(authorPerson);
 
-		final Code contentType = new Code("71388002", "2.16.840.1.113883.6.96", "Procedure (procedure)");
-		submissionSetMetadata.setContentTypeCode(contentType);
+        final Code contentType = new Code("71388002", "2.16.840.1.113883.6.96", "Procedure (procedure)");
+        submissionSetMetadata.setContentTypeCode(contentType);
 
-		// Use author data from document metadata
-		submissionSetMetadata.setAuthor(author);
+        // Use author data from document metadata
+        submissionSetMetadata.setAuthor(author);
 
-		// Security shall be an Assertion as retrieved from XuaClient (see XUAClientTest)
-		Assertion assertion = new AssertionBuilderImpl().version("2.0").id(UUID.randomUUID().toString())
-				.issueInstant(new GregorianCalendar()).create();
+        // Get the X-User Assertion to authorize the Document Submission.
+        Assertion xUserAssertion = getXUserAssertion();
+        assertNotNull(xUserAssertion);
 
-		var nameIdAdd = new NameIDType();
-		nameIdAdd.setValue("urn:oid:1.3.6.1.4.1.21367.2017.2.6.2");
-		nameIdAdd.setNameQualifier("urn:e-health-suisse:community-index");
-		assertion.setIssuer(nameIdAdd);
+        // provide and register the document
+        final Response response = convenienceCommunication.submit(submissionSetMetadata, xUserAssertion, null);
 
-		// provide and register the document
-		final Response response = convenienceCommunication.submit(submissionSetMetadata, assertion, null);
+        // checks whether the document has been successfully submitted
+        assertTrue(response.getErrors().isEmpty());
+        assertEquals(Status.SUCCESS, response.getStatus());
+    }
 
-		// checks whether the document has been successfully submitted
-		assertTrue(response.getErrors().isEmpty());
-		assertEquals(Status.SUCCESS, response.getStatus());
-	}
+    /**
+     * Retrieve an X-User Assertion for a Healthcare Provider from the test environment. In this test the IdP Assertion
+     * required to authenticate the user in the Get X-User Assertion request is loaded from the disk
+     *
+     * @throws Exception if something unexpected happens
+     */
+    private Assertion getXUserAssertion() throws Exception {
+
+        final String urlToXua = "https://ehealthsuisse.ihe-europe.net:10443/STS?wsdl";
+        final String clientKeyStore = "src/test/resources/testKeystoreXua.jks";
+        final String clientKeyStorePass = "changeit";
+
+        // initialize XUA client to query XUA assertion
+        XuaClientConfig xuaClientConfig = new XuaClientConfigBuilderImpl().clientKeyStore(clientKeyStore)
+                .clientKeyStorePassword(clientKeyStorePass).clientKeyStoreType("jks").url(urlToXua).create();
+
+        XuaClient client = ClientFactory.getXuaClient(xuaClientConfig);
+
+        try (InputStream is = new FileInputStream(new File("src/test/resources/IdPAssertion.xml"))) {
+
+            // for testing load an IdP Assertion from disk.
+            var idpAssertion = new AssertionDeserializerImpl().fromXmlByteArray(IOUtils.toByteArray(is));
+
+            // define the attributes for the X-User Assertion request
+            var role = new CodedWithEquivalentsBuilder().code("HCP").codeSystem("2.16.756.5.30.1.127.3.10.6")
+                    .displayName("Behandelnde(r)")
+                    .buildObject(Role.DEFAULT_NS_URI, Role.DEFAULT_ELEMENT_LOCAL_NAME, Role.DEFAULT_PREFIX);
+
+            var purposeOfUse = new CodedWithEquivalentsBuilder().code("NORM").codeSystem("2.16.756.5.30.1.127.3.10.6")
+                    .displayName("Normal Access").buildObject(PurposeOfUse.DEFAULT_NS_URI,
+                            PurposeOfUse.DEFAULT_ELEMENT_LOCAL_NAME, PurposeOfUse.DEFAULT_PREFIX);
+
+            String eprSpid = "761337610411265304^^^SPID&2.16.756.5.30.1.127.3.10.3&ISO";
+
+            // build the  X-User Assertion request
+            var assertionRequest = new XUserAssertionRequestBuilderImpl().requestType(RequestType.WST_ISSUE)
+                    .tokenType(TokenType.OASIS_WSS_SAML_PROFILE_11_SAMLV20)
+                    .appliesTo(new AppliesToBuilderImpl().address("https://localhost:17001/services/iti18").create())
+                    .purposeOfUse(purposeOfUse).subjectRole(role).resourceId(eprSpid).create();
+
+            // query the  X-User Assertion
+            List<XUserAssertionResponse> response = client.send(idpAssertion, assertionRequest);
+            return response.get(0).getAssertion();
+        }
+    }
 
 }
