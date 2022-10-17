131f41a246d865c13d6f103deb001a5c8ceb276d
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-10-17 12:28:17 +0200

    Work

--- husky-fhir/husky-fhir-emed-ch/husky-fhir-emed-ch-pmp/src/main/java/org/projecthusky/fhir/emed/ch/pmp/models/common/PatientDigest.java
+++ husky-fhir/husky-fhir-emed-ch/husky-fhir-emed-ch-pmp/src/main/java/org/projecthusky/fhir/emed/ch/pmp/models/common/PatientDigest.java
@@ -12,7 +12,6 @@ package org.projecthusky.fhir.emed.ch.pmp.models.common;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.projecthusky.common.enums.AdministrativeGender;
 
 import javax.annotation.concurrent.Immutable;
 import java.time.LocalDate;
@@ -26,16 +25,17 @@ import java.util.Objects;
  * Creating these digests is typically done from a CDA-CH-EMED document with the digesters.
  *
  * @param ids        The list of patient Ids.
- * @param givenName  The legal given name.
- * @param familyName The legal family name.
+ * @param givenNames The given name(s).
+ * @param familyName The family name or {@code null}.
  * @param gender     The gender or {@code null}.
  * @param birthdate  The birthdate or {@code null}.
  * @author Quentin Ligier
+ * @todo use the FHIR gender enum.
  */
 @Immutable
 public record PatientDigest(List<@NonNull QualifiedIdentifier> ids,
-                            String givenName,
-                            String familyName,
+                            List<@NonNull String> givenNames,
+                            @Nullable String familyName,
                             @Nullable AdministrativeGender gender,
                             @Nullable LocalDate birthdate) {
 
@@ -43,18 +43,18 @@ public record PatientDigest(List<@NonNull QualifiedIdentifier> ids,
      * Constructor.
      *
      * @param ids        The list of patient Ids.
-     * @param givenName  The legal given name.
-     * @param familyName The legal family name.
+     * @param givenNames  The legal given name(s).
+     * @param familyName The legal family name or {@code null}.
      * @param gender     The gender or {@code null}.
      * @param birthdate  The birthdate or {@code null}.
      */
     public PatientDigest(final List<@NonNull QualifiedIdentifier> ids,
-                         final String givenName,
-                         final String familyName,
+                         final List<@NonNull String> givenNames,
+                         @Nullable final String familyName,
                          @Nullable final AdministrativeGender gender,
                          @Nullable final LocalDate birthdate) {
         this.ids = Collections.unmodifiableList(Objects.requireNonNull(ids));
-        this.givenName = Objects.requireNonNull(givenName);
+        this.givenNames = Objects.requireNonNull(givenNames);
         this.familyName = Objects.requireNonNull(familyName);
         this.gender = gender;
         this.birthdate = birthdate;
@@ -64,14 +64,14 @@ public record PatientDigest(List<@NonNull QualifiedIdentifier> ids,
      * Returns the "usual" human name.
      */
     public String getHumanName() {
-        return String.join(" ", this.givenName, this.familyName);
+        return String.join(" ", String.join(" ", this.givenNames), this.familyName);
     }
 
     @Override
     public String toString() {
         return "PatientDigest{" +
                 "ids=" + ids +
-                ", givenName='" + givenName + '\'' +
+                ", givenNames='" + givenNames + '\'' +
                 ", familyName='" + familyName + '\'' +
                 ", gender=" + gender +
                 ", birthdate=" + birthdate +
