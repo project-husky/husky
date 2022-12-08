36c6da12d9f71543a06f4f4cdb3107d88bdd0202
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-12-08 16:13:50 +0100

    Work on ChCoreAddressAdapter

---
+++ husky-fhir/husky-fhir-emed-ch/husky-fhir-emed-ch-common/src/main/java/org/projecthusky/fhir/emed/ch/common/datatype/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * This code is made available under the terms of the Eclipse Public License v1.0
+ * in the github project https://github.com/project-husky/husky there you also
+ * find a list of the contributors and the license information.
+ *
+ * This project has been developed further and modified by the joined working group Husky
+ * on the basis of the eHealth Connector opensource project from June 28, 2021,
+ * whereas medshare GmbH is the initial and main contributor/author of the eHealth Connector.
+ *
+ */
+
+/**
+ * Adapters for the various datatypes used in CH-EMED. Because of limitations of the HAPI parser, these must be adapters
+ * and not actual models.
+ *
+ * @author Quentin Ligier
+ **/
+package org.projecthusky.fhir.emed.ch.common.datatype;
