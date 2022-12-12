1e3706f1c9d83ccf4743a45d5caaea4c400ef487
Ronaldo Loureiro <ronaldo.loureiropinto@hcuge.ch>
2022-12-12 17:35:15 +0100

    add mhd xds converter

---
+++ husky-communication/husky-xds-mhd-conversion/src/main/java/org/projecthusky/communication/xdsmhdconversion/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * <p>This package contains classes to handle conversions between XDS and MHD
+ *
+ * <p>By default, parameters and return values are annotated with
+ * {@link org.checkerframework.checker.nullness.qual.NonNull}. Nullable values are specifically annotated with
+ * {@link org.checkerframework.checker.nullness.qual.Nullable}.
+ *
+ * @author Ronaldo Loureiro
+ */
+@DefaultQualifier(value = NonNull.class, locations = {TypeUseLocation.PARAMETER, TypeUseLocation.RETURN,
+        TypeUseLocation.FIELD})
+
+package org.projecthusky.communication.xdsmhdconversion;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.checkerframework.framework.qual.TypeUseLocation;
