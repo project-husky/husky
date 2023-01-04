bbed801b93c1362d7c295369ade8f0c1926df6e6
Quentin Ligier <quentin.ligier@bluewin.ch>
2023-01-04 18:12:34 +0100

    CH:XUA: fix test

--- husky-communication/husky-xua/husky-xua-ch-impl/src/test/java/org/projecthusky/xua/validation/condition/ChEprDelegationRestrictionConditionValidatorTest.java
+++ husky-communication/husky-xua/husky-xua-ch-impl/src/test/java/org/projecthusky/xua/validation/condition/ChEprDelegationRestrictionConditionValidatorTest.java
@@ -10,7 +10,6 @@
 package org.projecthusky.xua.validation.condition;
 
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
-import org.projecthusky.communication.ch.enums.stable.Role;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -22,14 +21,15 @@ import org.opensaml.saml.common.assertion.ValidationContext;
 import org.opensaml.saml.common.assertion.ValidationResult;
 import org.opensaml.saml.ext.saml2delrestrict.DelegationRestrictionType;
 import org.opensaml.saml.saml2.assertion.ConditionValidator;
+import org.projecthusky.communication.ch.enums.stable.Role;
 import org.w3c.dom.Element;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-import static org.projecthusky.xua.validation.ChEprAssertionValidationParameters.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.projecthusky.xua.validation.ChEprAssertionValidationParameters.*;
 
 /**
  * Tests for {@link ChEprDelegationRestrictionConditionValidator}.
@@ -41,7 +41,7 @@ class ChEprDelegationRestrictionConditionValidatorTest {
 
     private static final ConditionValidator VALIDATOR = new ChEprDelegationRestrictionConditionValidator();
     @MonotonicNonNull
-    private static Unmarshaller UNMARSHALLER = null;
+    private static Unmarshaller UNMARSHALLER;
 
     @BeforeAll
     public static void setUpBeforeClass() throws Exception {
@@ -58,10 +58,10 @@ class ChEprDelegationRestrictionConditionValidatorTest {
     @SuppressWarnings("java:S5961")
     void testValidate() throws Exception {
         var condition = this.unmarshal("""
-                <del:Delegate>
-                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
-                </del:Delegate>
-        """);
+                                                       <del:Delegate>
+                                                           <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
+                                                       </del:Delegate>
+                                               """);
 
         ValidationResult result;
         ValidationContext context;
@@ -81,16 +81,17 @@ class ChEprDelegationRestrictionConditionValidatorTest {
         assertNull(context.getDynamicParameters().get(CH_EPR_TCU_ID));
 
         condition = this.unmarshal("""
-                <del:Delegate>
-                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:e-health-suisse:technical-user-id">2000000090108</saml2:NameID>
-                </del:Delegate>
-        """);
+                                               <del:Delegate>
+                                                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"
+                                                                  NameQualifier="urn:e-health-suisse:technical-user-id">1.2.3</saml2:NameID>
+                                              </del:Delegate>
+                                           """);
         context = this.getContext(Role.TECHNICAL_USER);
         result = VALIDATOR.validate(condition, null, context);
         assertEquals(ValidationResult.VALID, result);
         assertNull(context.getValidationFailureMessage());
         assertNull(context.getDynamicParameters().get(CH_EPR_ASSISTANT_GLN));
-        assertNull(context.getDynamicParameters().get(CH_EPR_TCU_ID));
+        assertEquals("1.2.3", context.getDynamicParameters().get(CH_EPR_TCU_ID));
 
         context = this.getContext(Role.POLICY_ADMINISTRATOR);
         result = VALIDATOR.validate(condition, null, context);
@@ -121,10 +122,10 @@ class ChEprDelegationRestrictionConditionValidatorTest {
         assertNull(context.getDynamicParameters().get(CH_EPR_TCU_ID));
 
         condition = this.unmarshal("""
-                <del:Delegate>
-                    <saml2:NameID NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
-                </del:Delegate>
-        """);
+                                                   <del:Delegate>
+                                                       <saml2:NameID NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
+                                                   </del:Delegate>
+                                           """);
         context = this.getContext(Role.ASSISTANT);
         result = VALIDATOR.validate(condition, null, context);
         assertEquals(ValidationResult.INVALID, result);
@@ -133,13 +134,13 @@ class ChEprDelegationRestrictionConditionValidatorTest {
         assertNull(context.getDynamicParameters().get(CH_EPR_TCU_ID));
 
         condition = this.unmarshal("""
-                <del:Delegate>
-                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
-                </del:Delegate>
-                <del:Delegate>
-                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
-                </del:Delegate>
-        """);
+                                                   <del:Delegate>
+                                                       <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
+                                                   </del:Delegate>
+                                                   <del:Delegate>
+                                                       <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
+                                                   </del:Delegate>
+                                           """);
         context = this.getContext(Role.ASSISTANT);
         result = VALIDATOR.validate(condition, null, context);
         assertEquals(ValidationResult.INVALID, result);
@@ -148,10 +149,10 @@ class ChEprDelegationRestrictionConditionValidatorTest {
         assertNull(context.getDynamicParameters().get(CH_EPR_TCU_ID));
 
         condition = this.unmarshal("""
-                <del:Delegate>
-                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:e-health-suisse:technical-user-id">2000000090108</saml2:NameID>
-                </del:Delegate>
-        """);
+                                                   <del:Delegate>
+                                                       <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:e-health-suisse:technical-user-id">2000000090108</saml2:NameID>
+                                                   </del:Delegate>
+                                           """);
         context = this.getContext(Role.ASSISTANT);
         result = VALIDATOR.validate(condition, null, context);
         assertEquals(ValidationResult.INVALID, result);
@@ -160,10 +161,10 @@ class ChEprDelegationRestrictionConditionValidatorTest {
         assertNull(context.getDynamicParameters().get(CH_EPR_TCU_ID));
 
         condition = this.unmarshal("""
-                <del:Delegate>
-                    <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
-                </del:Delegate>
-        """);
+                                                   <del:Delegate>
+                                                       <saml2:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent" NameQualifier="urn:gs1:gln">2000000090108</saml2:NameID>
+                                                   </del:Delegate>
+                                           """);
         context = this.getContext(Role.TECHNICAL_USER);
         result = VALIDATOR.validate(condition, null, context);
         assertEquals(ValidationResult.INVALID, result);
@@ -179,7 +180,8 @@ class ChEprDelegationRestrictionConditionValidatorTest {
     }
 
     private DelegationRestrictionType unmarshal(final String xml) throws Exception {
-        final var bytes = ("<saml2:Condition xsi:type=\"del:DelegationRestrictionType\" xmlns:del=\"urn:oasis:names:tc:SAML:2.0:conditions:delegation\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:saml2=\"urn:oasis:names:tc:SAML:2.0:assertion\">" + xml + "</saml2:Condition>").getBytes(StandardCharsets.UTF_8);
+        final var bytes = ("<saml2:Condition xsi:type=\"del:DelegationRestrictionType\" xmlns:del=\"urn:oasis:names:tc:SAML:2.0:conditions:delegation\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:saml2=\"urn:oasis:names:tc:SAML:2.0:assertion\">" + xml + "</saml2:Condition>").getBytes(
+                StandardCharsets.UTF_8);
         Element element = Objects.requireNonNull(XMLObjectProviderRegistrySupport.getParserPool())
                 .parse(new ByteArrayInputStream(bytes))
                 .getDocumentElement();
