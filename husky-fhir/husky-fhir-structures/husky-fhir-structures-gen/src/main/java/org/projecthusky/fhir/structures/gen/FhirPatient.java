d5ba7eb3c5afc4291264b88c4bf78555f2224be3
Roeland Luykx <roeland.luykx@raly.ch>
2024-05-16 06:25:27 +0200

    add missing import

--- husky-fhir/husky-fhir-structures/husky-fhir-structures-gen/src/main/java/org/projecthusky/fhir/structures/gen/FhirPatient.java
+++ husky-fhir/husky-fhir-structures/husky-fhir-structures-gen/src/main/java/org/projecthusky/fhir/structures/gen/FhirPatient.java
@@ -14,6 +14,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.hl7.fhir.r4.model.Address;
 import org.hl7.fhir.r4.model.Address.AddressUse;
