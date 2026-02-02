0e2cfc995de341267afab490003e54f50f35d45c
dvribeira <daniel.vazquezrivera@hug.ch>
2026-02-02 15:07:49 +0100

    [#312] [CH Core, CH EMED EPR] Added copy and copyValues methods to ChCoreRelatedPerson

--- husky-fhir/husky-fhir-core-ch/src/main/java/org/projecthusky/fhir/core/ch/resource/r4/ChCoreRelatedPerson.java
+++ husky-fhir/husky-fhir-core-ch/src/main/java/org/projecthusky/fhir/core/ch/resource/r4/ChCoreRelatedPerson.java
@@ -7,6 +7,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 import org.hl7.fhir.r4.model.RelatedPerson;
 import org.projecthusky.fhir.core.ch.resource.extension.r4.ChCoreCitizenshipExt;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,4 +22,22 @@ public class ChCoreRelatedPerson extends RelatedPerson implements ChCoreContactP
     @Child(name = "citizenship", min = 0, max = Child.MAX_UNLIMITED)
     @Extension(url = "http://hl7.org/fhir/StructureDefinition/patient-citizenship", definedLocally = false)
     protected List<ChCoreCitizenshipExt> citizenship;
+
+    @Override
+    public ChCoreRelatedPerson copy() {
+        ChCoreRelatedPerson dst = new ChCoreRelatedPerson();
+        this.copyValues(dst);
+        return dst;
+    }
+
+    @Override
+    public void copyValues(RelatedPerson dst) {
+        super.copyValues(dst);
+        if (dst instanceof  ChCoreRelatedPerson chCoreRelatedPerson) {
+            if (citizenship != null) {
+                chCoreRelatedPerson.citizenship = new ArrayList<>();
+                for (final var citizenship : citizenship) chCoreRelatedPerson.citizenship.add(citizenship.copy());
+            }
+        }
+    }
 }
